a1888be8d475cf3b7b801c4d1600beb9ebce2653
Vladimir Glinskikh <support@zhelezyaka.kz>
2018-08-21 15:48:02 +0600

    correct

--- src/main/java/quiz05/shift/AppOne.java
+++ src/main/java/quiz05/shift/AppOne.java
@@ -2,7 +2,7 @@ package quiz05.shift;
 
 public class AppOne {
     public static void main(String[] args) {
-        int x = 0b10101111_11111111_11111111_11110101 << 1;
-        System.out.println(Integer.toBinaryString(x));
+        int x = 0b10101111_11111111_11111111_11110101 >> 1;
+        System.out.println(Integer.toString(x, 2));
     }
 }
