2401317c88b7e5f57fb1b41031c752ebf9fac7b0
Vladimir Glinskikh <support@zhelezyaka.kz>
2018-08-21 13:45:57 +0600

    correct

---
+++ src/main/java/quiz05/AppOne.java
@@ -0,0 +1,12 @@
+package quiz05;
+
+public class AppOne {
+    public static void main(String[] args) {
+        int x = 0, y = 0, z = 0, t = 0;
+
+        if ((x++ != 0 && y++ != 0) & (z++ != 0 & t++ != 0)) {
+
+        }
+        System.out.print(x + "" + y + "" + z + "" + t);
+    }
+}
