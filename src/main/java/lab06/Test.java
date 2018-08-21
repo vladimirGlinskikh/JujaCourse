44ff5c277dd27b02c60aabc96f99e01ceac5f71e
Vladimir Glinskikh <support@zhelezyaka.kz>
2018-08-21 19:27:47 +0600

    correct

---
+++ src/main/java/lab06/Test.java
@@ -0,0 +1,13 @@
+package lab06;
+
+public class Test {
+    public static void main(String[] args) {
+        if (IntegerUtils.rightShift(
+                0b00111000_11111111_00000000_10101010 ) !=
+                0b00011100_01111111_10000000_01010101) {
+            throw new AssertionError();
+        }
+
+        System.out.print("OK");
+    }
+}
