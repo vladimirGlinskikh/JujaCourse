16594bb551de0d84d9b215d23c0b964d61f64b49
Vladimir Glinskikh <support@zhelezyaka.kz>
2018-09-09 00:45:18 +0600

    lab20 No checked.

---
+++ src/main/java/laboratoryWork/lab20/Permutator.java
@@ -0,0 +1,28 @@
+package laboratoryWork.lab20;
+
+import java.util.Arrays;
+//реализация первого варианта. Не проходит валидацию.
+public class Permutator {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3};
+        permutation(arr, arr.length);
+    }
+
+    public static void permutation(int[] list, int size) {
+        if (size < 2) {
+            System.out.println(Arrays.toString(list));
+        } else {
+            for (int k = 0; k < size; k++) {
+                swap(list, k, size - 1);
+                permutation(list, size - 1);
+                swap(list, k, size - 1);
+            }
+        }
+    }
+
+    private static void swap(int[] arr, int index0, int index1) {
+        int tmp = arr[index0];
+        arr[index0] = arr[index1];
+        arr[index1] = tmp;
+    }
+}
