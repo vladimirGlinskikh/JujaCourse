e782dbbee6709adc98b9c44b826e97222de008f6
Vladimir Glinskikh <support@zhelezyaka.kz>
2018-09-20 10:19:49 +0600

    quiz14/2 checked

---
+++ src/main/java/quizs/quiz14/AppTwo.java
@@ -0,0 +1,20 @@
+package quizs.quiz14;
+
+public class AppTwo {
+    public static int a = 0;
+    public int b = 0;
+
+    public AppTwo() {
+        this.b = a++;
+    }
+
+//    public static int get() {
+//        return b++ + b++;
+//    }
+
+    public static void main(String[] args) {
+//        System.out.println(get());
+//        System.out.println(get());
+//        System.out.println(get());
+    }
+}
