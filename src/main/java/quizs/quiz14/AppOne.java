48efdb61e8bfb62c2168d24c18185088947c3b9a
Vladimir Glinskikh <support@zhelezyaka.kz>
2018-09-20 09:58:17 +0600

    quiz14 checked

---
+++ src/main/java/quizs/quiz14/AppOne.java
@@ -0,0 +1,13 @@
+package quizs.quiz14;
+
+public class AppOne {
+    public static int counter = 0;
+
+    public int getCounter() {
+        return counter;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
