d4e33355075500f0493bd7bb8392594431774fcc
Vladimir Glinskikh <support@zhelezyaka.kz>
2018-09-21 10:43:35 +0600

    quiz16/4 checked

--- src/main/java/quizs/quiz16/App.java
+++ src/main/java/quizs/quiz16/App.java
@@ -2,5 +2,15 @@ package quizs.quiz16;
 
 abstract class App {
     public abstract void f();
+
+    public static void main(String[] args) {
+        g(new Object[16], new App[16]);
+    }
+
+    private static void g(Object[] src, App[] dst) {
+        for (int k = 0; k < src.length; k++) {
+            dst[k] = (App) src[k];
+        }
+    }
 }
 
