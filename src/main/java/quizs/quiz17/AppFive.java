c058af357a4318ccc43f96e7143e4a8dc16b3b24
Vladimir Glinskikh <support@zhelezyaka.kz>
2018-09-21 15:25:38 +0600

    quiz17/5 checked

---
+++ src/main/java/quizs/quiz17/AppFive.java
@@ -0,0 +1,11 @@
+package quizs.quiz17;
+
+public class AppFive {
+    public void f() {
+//        Map map = new LinkedHashMap() {
+//            protected boolean removeEldestEntry() {
+//                return true;
+//            }
+        };
+    }
+
