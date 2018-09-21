784b8d119eba559605542f806618242a60eb4b6a
Vladimir Glinskikh <support@zhelezyaka.kz>
2018-09-21 10:04:41 +0600

    quiz16 checked

---
+++ src/main/java/quizs/quiz16/App.java
@@ -0,0 +1,6 @@
+package quizs.quiz16;
+
+abstract class App {
+    public abstract void f();
+}
+
---
+++ src/main/java/quizs/quiz16/TestXX.java
@@ -0,0 +1,8 @@
+package quizs.quiz16;
+
+class TestXX {
+    public static void main(String[] args) {
+        App[] ref = new App[16];
+        System.out.println(ref);
+    }
+}
