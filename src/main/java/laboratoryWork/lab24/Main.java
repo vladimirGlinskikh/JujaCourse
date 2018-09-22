cb12d5bd58fd8c0f38c0212683e0fc5be4099b66
Vladimir Glinskikh <support@zhelezyaka.kz>
2018-09-23 05:17:42 +0600

    Added text output to the screen in Main.

--- src/main/java/laboratoryWork/lab24/Main.java
+++ src/main/java/laboratoryWork/lab24/Main.java
@@ -2,23 +2,25 @@ package laboratoryWork.lab24;
 
 public class Main {
     public static void main(String[] args) {
-        String testNameBook = "TestNameBook";
-        String testAuthorBook = "TestBookAuthor";
-        int countPages = 100;
-        String expectedBookPrint = "Book{name=TestNameBook,countPages=100,author=TestBookAuthor}";
+//        String testNameBook = "TestNameBook";
+//        String testAuthorBook = "TestBookAuthor";
+//        int countPages = 100;
+//        String expectedBookPrint = "Book{name=TestNameBook,countPages=100,author=TestBookAuthor}";
+//
+//        Issue book = new Book(testNameBook, countPages, testAuthorBook);
+//
+//        String actualBookPrint = book.toPrint();
+//
+//        //check
+//        if (actualBookPrint == null)
+//            throw new AssertionError("Result cannot be null");
+//
+//        if (actualBookPrint.equals(expectedBookPrint))
+//            System.out.print("OK");
+//        else
+//            throw new AssertionError("Expected " + expectedBookPrint + " but found " + actualBookPrint);
 
-        Issue book = new Book(testNameBook, countPages, testAuthorBook);
-
-        String actualBookPrint = book.toPrint();
-
-        //check
-        if (actualBookPrint == null)
-            throw new AssertionError("Result cannot be null");
-
-        if (actualBookPrint.equals(expectedBookPrint))
-            System.out.print("OK");
-        else
-            throw new AssertionError("Expected " + expectedBookPrint + " but found " + actualBookPrint);
-
+        Book book = new Book("MyBook", 100, "Vladimir Glinskikh");
+        System.out.println(book.toPrint());
     }
 }
