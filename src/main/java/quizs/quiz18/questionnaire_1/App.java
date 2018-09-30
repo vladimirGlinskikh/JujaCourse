059a4a6cc9209ac7a7df76ed851fe61af9f1bea5
Vladimir Glinskikh <support@zhelezyaka.kz>
2018-09-30 18:50:59 +0600

    quiz18 questionnaire_1 done

---
+++ src/main/java/quizs/quiz18/questionnaire_1/App.java
@@ -0,0 +1,13 @@
+package quizs.quiz18.questionnaire_1;
+
+public class App {
+    static Holder i = new Holder(0);
+    static Holder j = new Holder(1);
+    public static void main(String[] args) {
+
+        System.out.println(i);
+        System.out.println(j);
+        //System.out.println(j);
+        //произвольные в heap
+    }
+}
---
+++ src/main/java/quizs/quiz18/questionnaire_1/Holder.java
@@ -0,0 +1,16 @@
+package quizs.quiz18.questionnaire_1;
+
+public class Holder {
+    int data;
+//    int i;
+//    int[] j;
+
+    public Holder(int data) {
+        this.data = data;
+    }
+
+//    public Holder(int i, int j) {
+//        this.i = i;
+//        this.j = new int[]{j};
+//    }
+}
