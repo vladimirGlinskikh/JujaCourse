af63d0399dbb216a2b6793b7e7391dcce353f698
Vladimir Glinskikh <support@zhelezyaka.kz>
2018-10-06 12:41:38 +0600

    quiz18 Stack checked

---
+++ src/main/java/quizs/quiz18/stack/App0.java
@@ -0,0 +1,12 @@
+package quizs.quiz18.stack;
+
+public class App0 {
+    int fun(int k0, int k1, int k2) {
+        return fun(k0, k1, k2);
+    }
+
+    public static void main(String[] args) {
+        App0 app0 = new App0();
+        System.out.println(app0.fun(1, 2, 3));
+    }
+}
---
+++ src/main/java/quizs/quiz18/stack/App1.java
@@ -0,0 +1,12 @@
+package quizs.quiz18.stack;
+
+public class App1 {
+    int fun(long k0, long k1, long k2) {
+        return fun(k0, k1, k2);
+    }
+
+    public static void main(String[] args) {
+        App1 app1 = new App1();
+        System.out.println(app1.fun(1, 2, 3));
+    }
+}
