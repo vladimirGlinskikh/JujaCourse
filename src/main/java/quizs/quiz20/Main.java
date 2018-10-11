94e1f78aa75966e783a992e9121d6f92e29e7eea
Vladimir Glinskikh <support@zhelezyaka.kz>
2018-10-11 16:02:43 +0600

    quiz20 checked

---
+++ src/main/java/quizs/quiz20/Main.java
@@ -0,0 +1,9 @@
+package quizs.quiz20;
+
+public class Main {
+    public static void main(String[] args) {
+        Integer i0 = 200;
+        Integer i1 = Integer.valueOf(200);
+        System.out.println(i0 == i1);
+    }
+}
