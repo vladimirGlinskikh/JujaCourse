5bb6177cd2d1702a54f19269be51b5fe96088d0b
Vladimir Glinskikh <support@zhelezyaka.kz>
2018-10-12 17:57:22 +0600

    lab33 not work

---
+++ src/main/java/laboratoryWork/lab33/FractionUtils.java
@@ -0,0 +1,13 @@
+package laboratoryWork.lab33;
+
+import java.math.BigInteger;
+
+public class FractionUtils {
+    public static BigInteger[] sum(BigInteger[] x, BigInteger[] y) {
+        //TODO some code
+        return x;
+    }
+
+    public static void main(String[] args) {
+    }
+}
