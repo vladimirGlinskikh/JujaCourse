22fc5f0fd58b967994122c7641f36fdbc110a0f6
Vladimir Glinskikh <support@zhelezyaka.kz>
2018-10-29 04:59:52 +0600

    quiz23 Application12 checked

---
+++ src/main/java/quizs/quiz23/Application12.java
@@ -0,0 +1,13 @@
+package quizs.quiz23;
+
+public class Application12 {
+    public static void main(String[] args) throws Throwable {
+        try {
+            System.err.print("0 ");// first step
+            throw new Throwable();// two step and finish
+        } catch (Error e) {
+            System.err.print("1 ");
+        }
+        System.err.print("2 ");
+    }
+}
