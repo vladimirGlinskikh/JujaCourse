24fd8ecf6c4c506696f156cd71b74ec6c9f75445
Vladimir Glinskikh <support@zhelezyaka.kz>
2018-10-28 13:20:53 +0600

    add step in quiz22 App

--- src/main/java/quizs/quiz22/App.java
+++ src/main/java/quizs/quiz22/App.java
@@ -2,22 +2,22 @@ package quizs.quiz22;
 
 public class App {
     public static void main(String[] args) {
-        System.err.println("f:before");
+        System.err.println("f:before");// first step
         try {
-            f();
+            f();// two step
         } catch (Error e) {
-            System.err.println("f:catch");
+            System.err.println("f:catch");// six step
         }
-        System.err.println("f:after");
+        System.err.println("f:after");// seven step
     }
 
     public static void f() {
-        System.err.println("g:before");
-        g();
+        System.err.println("g:before");// three step
+        g();// four step
         System.err.println("g:after");
     }
 
     public static void g() {
-        throw new Error();
+        throw new Error();// fivi step
     }
 }
