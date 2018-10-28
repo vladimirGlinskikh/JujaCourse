bf67ff5b675d127e290fe2048e6bf2e9acba4151
Vladimir Glinskikh <support@zhelezyaka.kz>
2018-10-29 04:50:31 +0600

    quiz23 Application8 checked

---
+++ src/main/java/quizs/quiz23/Application8.java
@@ -0,0 +1,13 @@
+package quizs.quiz23;
+
+public class Application8 {
+    public static void main(String[] args) throws Throwable {
+        try {
+            System.err.print("0 ");// first step
+            throw new RuntimeException();// two step and finish
+        } catch (Error e) {
+            System.err.print("1 ");
+        }
+        System.err.print("2 ");
+    }
+}
