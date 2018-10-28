cea7762165dd4197ef6b379cd89eb3405ee981d0
Vladimir Glinskikh <support@zhelezyaka.kz>
2018-10-29 04:38:33 +0600

    quiz23 Application6 checked

---
+++ src/main/java/quizs/quiz23/Application6.java
@@ -0,0 +1,13 @@
+package quizs.quiz23;
+
+public class Application6 {
+    public static void main(String[] args) throws Throwable {
+        try {
+            System.err.print("0 ");// first step
+            throw new Exception();// two step and finish
+        } catch (RuntimeException e) {
+            System.err.print("1 ");
+        }
+        System.err.print("2 ");
+    }
+}
