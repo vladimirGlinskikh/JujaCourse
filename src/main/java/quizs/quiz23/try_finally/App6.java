d3130caaa84a88ed9207ab8dfbb34e77ca10e3c6
VladimirGlinskikh <support@zhelezyaka.kz>
2018-10-30 11:14:30 +0600

    quiz23 try_finally App6 checked

---
+++ src/main/java/quizs/quiz23/try_finally/App6.java
@@ -0,0 +1,15 @@
+package quizs.quiz23.try_finally;
+
+public class App6 {
+//    public static void main(String[] args) {
+//        System.err.println(f());
+//    }
+//
+//    public static int f() {
+//        try {
+//            System.err.println(0);
+//        } finally {
+//            System.err.println(1);
+//        }
+//    }// This will not compile because no return value
+}
