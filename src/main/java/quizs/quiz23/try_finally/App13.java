31697312be1b5ae9e166ca87bbea48e6dce161c8
VladimirGlinskikh <support@zhelezyaka.kz>
2018-10-30 11:46:00 +0600

    quiz23 try_finally App13 checked

---
+++ src/main/java/quizs/quiz23/try_finally/App13.java
@@ -0,0 +1,16 @@
+package quizs.quiz23.try_finally;
+
+public class App13 {
+//    public static void main(String[] args) {
+//        System.err.println(f());
+//    }
+//
+//    public static int f() {
+//        try {
+//            return 0;
+//        } finally {
+//            throw new Error();
+//        }
+//        System.err.println(1);// Compile error!
+//    }
+}
