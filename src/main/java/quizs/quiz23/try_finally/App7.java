c9e04a4e77e404f61e7eaf99e946c188a16a1159
VladimirGlinskikh <support@zhelezyaka.kz>
2018-10-30 11:31:47 +0600

    quiz23 try_finally App7 checked

---
+++ src/main/java/quizs/quiz23/try_finally/App7.java
@@ -0,0 +1,15 @@
+package quizs.quiz23.try_finally;
+
+public class App7 {
+//    public static void main(String[] args) {
+//        System.err.println(f());
+//    }
+//    public static int f() {
+//        try {
+//            return 0;
+//        } finally {
+//            return 1;
+//        }
+//        return 0;// Will not compile, because the extra return statement.
+//    }
+}
