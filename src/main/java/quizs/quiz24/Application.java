9e38f37060137508687a9b41e8a07f372b6839fb
VladimirGlinskikh <support@zhelezyaka.kz>
2018-10-31 16:46:30 +0600

    quiz24 checked

---
+++ src/main/java/quizs/quiz24/Application.java
@@ -0,0 +1,11 @@
+package quizs.quiz24;
+
+public class Application {
+    public static void main(String[] args) throws Throwable {
+        g();
+    }
+
+    public static void g() throws Exception {
+
+    }
+}
