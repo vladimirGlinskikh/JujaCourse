aea575d27fd27a7c26adf2f88257b69612ad8821
Vladimir Glinskikh <support@zhelezyaka.kz>
2018-11-13 15:48:35 +0600

    quiz25 Set usage App2 checked

---
+++ src/main/java/quizs/quiz25/App2.java
@@ -0,0 +1,14 @@
+package quizs.quiz25;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class App2 {
+    public static void main(String[] args) {
+        Set<Integer> set = new HashSet<>();
+        set.add(set.size());
+        set.add(set.size());
+        set.add(set.size());
+        System.out.println(set.contains(set.size()));//false
+    }
+}
