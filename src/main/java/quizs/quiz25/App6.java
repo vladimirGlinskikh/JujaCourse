e184d9463aff521ac08d13ab67b29c26691cedb6
Vladimir Glinskikh <support@zhelezyaka.kz>
2018-11-13 16:01:28 +0600

    quiz25 Set usage App6 checked

---
+++ src/main/java/quizs/quiz25/App6.java
@@ -0,0 +1,14 @@
+package quizs.quiz25;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class App6 {
+    public static void main(String[] args) {
+        Set<Boolean> set = new HashSet<>();
+        set.addAll(Arrays.asList(set.isEmpty(), set.isEmpty()));
+        set.removeAll(Arrays.asList(set.containsAll(Arrays.asList(set.isEmpty(), set.isEmpty()))));
+        System.out.println(set.size());//1
+    }
+}
