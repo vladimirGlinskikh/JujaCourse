d223f82fce9fb618c212573acab2df4b1979511f
Vladimir Glinskikh <support@zhelezyaka.kz>
2018-11-17 18:18:24 +0600

    checked quiz25 App3 iterator println NoSuchElementExeption

---
+++ src/main/java/quizs/quiz25/iterator/App3.java
@@ -0,0 +1,18 @@
+package quizs.quiz25.iterator;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+public class App3 {
+    public static void main(String[] args) {
+        List<String> listA = Arrays.asList("A", "B", "C");
+        List<Integer> listB = Arrays.asList(1, 2);
+        for (Iterator<String> iterA = listA.iterator(); iterA.hasNext(); ) {
+            for (Iterator<Integer> iterB = listB.iterator(); iterB.hasNext(); ) {
+                System.out.print(iterA.next() + iterB.next() + " ");
+            }
+            System.out.println();
+        }
+    }
+}
