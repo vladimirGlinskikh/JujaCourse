49d62f7a9b18691ad717ef535d4f52027be8a4e8
Vladimir Glinskikh <support@zhelezyaka.kz>
2018-11-18 18:22:12 +0600

    checked quiz25 App10 iterator println A1 A2 A3 B1 B2 B3 C1 C2 C3

---
+++ src/main/java/quizs/quiz25/iterator/App10.java
@@ -0,0 +1,17 @@
+package quizs.quiz25.iterator;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class App10 {
+    public static void main(String[] args) {
+        List<String> listA = Arrays.asList("A", "B", "C");
+        List<Integer> listB = Arrays.asList(1, 2, 3);
+        for (String aListA : listA) {
+            for (Integer aListB : listB) {
+                System.out.print(aListA + aListB + " ");
+            }
+            System.out.println();
+        }
+    }
+}
