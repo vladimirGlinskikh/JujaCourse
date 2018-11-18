3fcb609d9fcd7a846f5076dedaf705a5e1d29d4a
Vladimir Glinskikh <support@zhelezyaka.kz>
2018-11-18 18:16:37 +0600

    checked quiz25 App7 iterator println A1 A2 A3 B1 B2 B3 C1 C2 C3

---
+++ src/main/java/quizs/quiz25/iterator/App7.java
@@ -0,0 +1,19 @@
+package quizs.quiz25.iterator;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+public class App7 {
+    public static void main(String[] args) {
+        List<String> listA = Arrays.asList("A", "B", "C");
+        List<Integer> listB = Arrays.asList(1, 2, 3);
+        for (Iterator<String> iterA = listA.iterator(); iterA.hasNext(); ) {
+            String elemA = iterA.next();
+            for (Iterator<Integer> iterB = listB.iterator(); iterB.hasNext(); ) {
+                System.out.print(elemA + iterB.next() + " ");
+            }
+            System.out.println();
+        }
+    }
+}
