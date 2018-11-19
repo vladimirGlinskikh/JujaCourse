eb1cdae1e94b57e14b6b75b9c94d9bad3cd825d9
Vladimir Glinskikh <support@zhelezyaka.kz>
2018-11-19 18:15:04 +0600

    quiz25 hashCodeEquals num2 add method Start

---
+++ src/main/java/quizs/quiz25/hashCodeEquals/num2/Start.java
@@ -0,0 +1,26 @@
+package quizs.quiz25.hashCodeEquals.num2;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Start {
+    public static void main(String[] args) {
+        Point point = new Point(1, 2);
+        Point point1 = new Point(1, 2);
+        if (point.equals(point1)) {
+            System.out.println("OK");
+            System.out.println(point.hashCode());
+            System.out.println(point1.hashCode());
+        } else {
+            System.out.println("Wrong!");
+            System.out.println(point.hashCode());
+            System.out.println(point1.hashCode());
+        }
+
+        Set<Point> set = new HashSet<>();
+        set.add(point);
+        set.add(point1);
+
+        System.out.println(set.size());
+    }
+}
