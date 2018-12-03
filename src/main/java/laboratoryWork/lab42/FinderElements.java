355185d0b79c6fabf1d5793e3e897de6cc35e0d2
Vladimir Glinskikh <support@zhelezyaka.kz>
2018-12-04 03:23:03 +0600

    lab42 checked

--- src/main/java/laboratoryWork/lab42/FinderElements.java
+++ src/main/java/laboratoryWork/lab42/FinderElements.java
@@ -1,7 +1,27 @@
 package laboratoryWork.lab42;
 
+import java.util.Iterator;
+
 class FinderElements {
     public static Integer finderElement(SinglyLinkedList<Integer> singlyLinkedList, int k) {
-        return null;
+        Iterator<Integer> iterator = singlyLinkedList.iterator();
+        int size = 0;
+        while (iterator.hasNext()) {
+            iterator.next();
+            size++;
+        }
+
+        int index = size - 1 - k;
+        if (index < 0)
+            throw new IndexOutOfBoundsException();
+        iterator = singlyLinkedList.iterator();
+        int count = 0;
+        while (iterator.hasNext()) {
+            int result = iterator.next();
+            if (index == count)
+                return result;
+            count++;
+        }
+        throw new IndexOutOfBoundsException();
     }
 }
