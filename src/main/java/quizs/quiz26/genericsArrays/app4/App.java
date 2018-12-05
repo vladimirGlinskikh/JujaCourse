0be28a0644a451273fd0930034dd93d56571447e
VladimirGlinskikh <support@zhelezyaka.kz>
2018-12-05 18:07:47 +0600

    Quiz26 Generics arrays. Quiz 4. In that case example is compiled.

---
+++ src/main/java/quizs/quiz26/genericsArrays/app4/App.java
@@ -0,0 +1,10 @@
+package quizs.quiz26.genericsArrays.app4;
+
+import java.util.ArrayList;
+
+public class App {
+    public static void main(String[] args) {
+        ArrayList<Object> list = new ArrayList<>();
+        // In that case example is compiled.
+    }
+}
