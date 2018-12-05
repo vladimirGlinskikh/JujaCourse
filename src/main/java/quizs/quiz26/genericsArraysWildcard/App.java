2f589b263a3629f05a929d88bd465c795346fd47
VladimirGlinskikh <support@zhelezyaka.kz>
2018-12-05 18:33:39 +0600

    Quiz26 Generics array wildcard. Checked.

---
+++ src/main/java/quizs/quiz26/genericsArraysWildcard/App.java
@@ -0,0 +1,24 @@
+package quizs.quiz26.genericsArraysWildcard;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) {
+        //List<? extends Object> list = new ArrayList<Integer>();
+        //ArrayList<? extends Object> list = new ArrayList<Object>();
+        //List<? extends Number> list = new ArrayList<>();
+        //ArrayList<? extends Number> list = new ArrayList<Number>();
+        //List<? extends Number> list = new ArrayList<Object>();
+        //ArrayList<? extends Integer> list = new ArrayList<Integer>();
+        //List<? extends Integer> list = new ArrayList<Object>();
+        //ArrayList<? super Object> list = new ArrayList<Integer>();
+        //ArrayList<? super Object> list = new ArrayList<Object>();
+        //List<? super Number> list = new ArrayList<Integer>();
+        //ArrayList<? super Number> list = new ArrayList<Number>();
+        //List<? super Number> list = new ArrayList<Object>();
+        //ArrayList<? super Integer> list = new ArrayList<Integer>();
+        //List<? super Integer> list = new ArrayList<Object>();
+    }
+}
