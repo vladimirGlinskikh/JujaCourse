2edecb2e8cedb62e6f105387dff13e723ec2a19a
VladimirGlinskikh <support@zhelezyaka.kz>
2018-12-05 18:10:46 +0600

    Quiz26 Generics arrays. Quiz 5. In that case example is no compiled. Required Object, but we are found String.

---
+++ src/main/java/quizs/quiz26/genericsArrays/app5/App.java
@@ -0,0 +1,11 @@
+package quizs.quiz26.genericsArrays.app5;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) {
+        //List<Object> list = new ArrayList<String>();
+        // In that case example is no compiled. Required Object, but we are found String.
+    }
+}
