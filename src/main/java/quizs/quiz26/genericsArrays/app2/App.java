8db459197d683132d7c5b54dacacd9e4b9b8ca69
VladimirGlinskikh <support@zhelezyaka.kz>
2018-12-05 18:06:07 +0600

    Quiz26 Generics arrays. Quiz 3. In that case example is no compiled.

---
+++ src/main/java/quizs/quiz26/genericsArrays/app2/App.java
@@ -0,0 +1,11 @@
+package quizs.quiz26.genericsArrays.app2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) {
+        List<Object> list = new ArrayList<>();
+        // In this case is compiled.
+    }
+}
---
+++ src/main/java/quizs/quiz26/genericsArrays/app3/App.java
@@ -0,0 +1,10 @@
+package quizs.quiz26.genericsArrays.app3;
+
+import java.util.ArrayList;
+
+public class App {
+    public static void main(String[] args) {
+        //ArrayList<Object> list = new ArrayList<String>();
+        // In that case example is no compiled. Required Object, but we are found String.
+    }
+}
