a1335158437b086a653d7e11724f2a1f664349c1
VladimirGlinskikh <support@zhelezyaka.kz>
2018-12-09 08:03:38 +0600

    quiz28 HashMap question2 checked

--- src/main/java/quizs/quiz28/hashMap/question1/App.java
+++ src/main/java/quizs/quiz28/hashMap/question1/App.java
@@ -1,15 +1,25 @@
 package quizs.quiz28.hashMap.question1;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 
 public class App {
     public static void main(String[] args) {
-        Map<String, Integer> map = new HashMap<>();
+        /*Map<String, Integer> map = new HashMap<>();
         map.put("A", 0);
         map.put("B", map.put("A", 0));
         map.put("C", map.put("B", 1));
         System.out.println(map.keySet().size());
-        //3
+        //3*/
+
+        /*Map<String, Integer> map = new HashMap<>();
+        map.put("A", 0);
+        map.put("B", map.put("A", 0));
+        map.put("C", map.put("B", 1));
+        System.out.println(new HashSet<>(map.values()).size());
+        //2*/
+
+
     }
 }
