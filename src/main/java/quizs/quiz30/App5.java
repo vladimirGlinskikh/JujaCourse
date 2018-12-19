9c1d559fc122d0b51375e1f4aace62626a777741
Vladimir Glinskikh <support@zhelezyaka.kz>
2018-12-19 17:39:20 +0600

    quiz 30 App5

--- src/main/java/quizs/quiz30/App5.java
+++ src/main/java/quizs/quiz30/App5.java
@@ -1,17 +1,11 @@
 package quizs.quiz30;
 
-import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.util.zip.GZIPOutputStream;
 
 public class App5 {
     public static void main(String[] args) throws IOException {
-//        new GZIPOutputStream(new byte[256]);
-        new GZIPOutputStream(new GZIPOutputStream(...));
-//        new GZIPOutputStream(new OutputStreamWriter(...));
-        new GZIPOutputStream(new DataOutputStream(...));
-        OutputStream ref = new GZIPOutputStream(...);
+//        new GZIPOutputStream(new GZIPOutputStream(...));
+//        new GZIPOutputStream(new DataOutputStream(...));
+//        OutputStream ref = new GZIPOutputStream(...);
     }
 }
