2198128e31702144350fdebd4578f786ee216a4e
Vladimir Glinskikh <support@zhelezyaka.kz>
2018-12-19 17:54:05 +0600

    quiz 30 since App5 at App22.

--- src/main/java/quizs/quiz30/App.java
+++ src/main/java/quizs/quiz30/App.java
@@ -4,8 +4,85 @@ import java.io.IOException;
 
 public class App {
     public static void main(String[] args) throws IOException {
+//        --App5--
 //        new GZIPOutputStream(new GZIPOutputStream(...));
 //        new GZIPOutputStream(new DataOutputStream(...));
 //        OutputStream ref = new GZIPOutputStream(...);
+//        --App6--
+//        new GZIPInputStream(new GZIPInputStream(...));
+//        new GZIPInputStream(new DataInputStream(...));
+//        InputStream ref = ...; new GZIPInputStream(ref);
+//        InputStream ref = new GZIPInputStream(...);
+//        --App7--
+//        new FileInputStream(new File(...));
+//        new FileInputStream(new String(...));
+//        InputStream ref = new FileInputStream(...);
+//        --App8--
+//        new FileOutputStream(new File(...));
+//        new FileOutputStream(new String(...));
+//        OutputStream ref = new FileOutputStream(...);
+//        --App9--
+//        new StringReader(new String(...));
+//        Reader ref = new StringReader(...);
+//        --App10--
+//        new StringWriter();
+//        Writer ref = new StringWriter(...);
+//        --App11--
+//        new DataInputStream(new BufferedInputStream(...));
+//        InputStream ref = ...; new DataInputStream(ref);
+//        InputStream ref = new DataInputStream(...);
+//        DataInput ref = new DataInputStream(...);
+//        --App12--
+//        new DataOutputStream(new BufferedOutputStream(...));
+//        OutputStream ref = ...; new DataOutputStream(ref);
+//        OutputStream ref = new DataOutputStream(...);
+//        DataOutput ref = new DataOutputStream(...);
+//        --App13--
+//        new InputStreamReader(new FileInputStream(...));
+//        Reader ref = new InputStreamReader(...);
+//        --App14--
+//        new OutputStreamWriter(new FileOutputStream(...));
+//        Writer ref = new OutputStreamWriter(...);
+//        --App15--
+//        new FileReader(new File(...));
+//        new FileReader(new String(...));
+//        Reader ref = new FileReader(...);
+//        --App16--
+//        new FileWriter(new File("..."));
+//        new FileWriter(new String(...));
+//        Writer ref = new FileWriter(...);
+//        --App17--
+//        new ByteArrayInputStream(new byte[256]);
+//        InputStream ref = new ByteArrayInputStream(...);
+//        --App18--
+//        new ByteArrayOutputStream();
+//        OutputStream ref = new ByteArrayOutputStream(...);
+//        --App19--
+//        new CharArrayReader(new char[256]);
+//        Reader ref = new CharArrayReader(...);
+//        --App20--
+//        new CharArrayWriter();
+//        Writer ref = new CharArrayWriter();
+//        --App21--
+//        new ObjectInputStream(new FileInputStream(...));
+//        InputStream ref = new ObjectInputStream(...);
+//        ObjectInput ref = new ObjectInputStream(...);
+//        --App22--
+//        new ObjectOutputStream(new FileOutputStream(...));
+//        OutputStream ref = new ObjectOutputStream(...);
+//        ObjectOutput ref = new ObjectOutputStream(...);
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
