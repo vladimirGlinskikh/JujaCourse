fbcb555c93722213e8ebfdf6709e3889a4d0376d
VladimirGlinskikh <support@zhelezyaka.kz>
2019-04-07 07:17:42 +0600

    quiz31 monitors2 is done.

---
+++ src/main/java/quizs/quiz31/monitors2/Application.java
@@ -0,0 +1,41 @@
+package quizs.quiz31.monitors2;
+
+import java.util.function.ObjIntConsumer;
+
+public class Application {
+    public static void main(String[] args) throws Exception {
+//        Object x = new Object();
+//        Object y = new Object();
+//        synchronized (x) {
+//            y.notify();
+//        }
+//        будет выброшено IllegalMonitorStateException
+
+
+//        Object x = new Object();
+//        Object y = x;
+//        synchronized (x) {
+//            y.notify();
+//        }
+//        ничего не произойдет, выполнится и завершится
+
+//        Object x = new Object();
+//        Object y = new Object();
+//        synchronized (x) {
+//            synchronized (y) {
+//                x.notify();
+//                y.notify();
+//            }
+//        }
+//        ничего не произойдет, выполнится и завершится
+
+
+//        Object x = new Object();
+//        synchronized (x) {
+//            synchronized (x) {
+//                x.notify();
+//            }
+//        }
+//        ничего не произойдет, выполнится и завершится
+    }
+}
