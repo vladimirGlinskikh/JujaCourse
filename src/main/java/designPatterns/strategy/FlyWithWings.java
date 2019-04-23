f11c8d5def6b7a21608fc8da8b2dccfeed8de6e4
Vladimir Glinskikh <support@zhelezyaka.kz>
2019-04-23 22:47:41 +0600

    реализация поведения для уток, которые умеют летать

---
+++ src/main/java/designPatterns/strategy/FlyWithWings.java
@@ -0,0 +1,9 @@
+package designPatterns.strategy;
+
+public class FlyWithWings implements FlyBehavior {
+
+    @Override
+    public void fly() {
+        System.out.println("I'm flying!");
+    }
+}
