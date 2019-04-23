15d47684e8e2bd17a03c212278a32f7a0b69419e
Vladimir Glinskikh <support@zhelezyaka.kz>
2019-04-23 22:39:18 +0600

    делегирование операции классам поведения через методы performFly() и performQuack()

--- src/main/java/designPatterns/strategy/Duck.java
+++ src/main/java/designPatterns/strategy/Duck.java
@@ -5,4 +5,20 @@ public abstract class Duck {
     FlyBehavior flyBehavior;
     QuackBehavior quackBehavior;
 
+    public Duck() {
+    }
+
+    public abstract void display();
+
+    public void performFly(){
+        flyBehavior.fly();
+    }
+
+    public void performQuack(){
+        flyBehavior.quack();
+    }
+
+    public void swim(){
+        System.out.println("All ducks float, even decoys!");
+    }
 }
