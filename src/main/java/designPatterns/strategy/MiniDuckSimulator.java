53ed0c9582c64ca6735bb9920547dc74b20952cc
Vladimir Glinskikh <support@zhelezyaka.kz>
2019-04-23 23:05:27 +0600

    Вызов метода performQuack(), унаследованного классом MallarDuck. Метод делегирует выполнение операции по ссылке на QuackBehavior (тоесть вызывает quack() через унаследованную переменную quackBehavior). Затем тоже самое происходит с методом performFly(), также унаследованным классом MallarDuck

---
+++ src/main/java/designPatterns/strategy/MiniDuckSimulator.java
@@ -0,0 +1,9 @@
+package designPatterns.strategy;
+
+public class MiniDuckSimulator {
+    public static void main(String[] args) {
+        Duck mallard = new MallardDuck();
+        mallard.performQuack();
+        mallard.performFly();
+    }
+}
