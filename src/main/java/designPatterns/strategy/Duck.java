e1611064add05893d50733de7c57c4c57ece8913
Vladimir Glinskikh <support@zhelezyaka.kz>
2019-04-24 08:41:31 +0600

    добавили два новых метода в класс Duck

--- src/main/java/designPatterns/strategy/Duck.java
+++ src/main/java/designPatterns/strategy/Duck.java
@@ -10,15 +10,23 @@ public abstract class Duck {
 
     public abstract void display();
 
-    public void performFly(){
+    public void performFly() {
         flyBehavior.fly();
     }
 
-    public void performQuack(){
+    public void performQuack() {
         quackBehavior.quack();
     }
 
-    public void swim(){
+    public void setFlyBehavior(FlyBehavior fb) {
+        flyBehavior = fb;
+    }
+
+    public void setQuackBehavior(QuackBehavior qb) {
+        quackBehavior = qb;
+    }
+
+    public void swim() {
         System.out.println("All ducks float, even decoys!");
     }
 }
