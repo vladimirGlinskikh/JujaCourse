3b953cf5865df8d75d09a9db7e0d28c67b3a1c48
Vladimir Glinskikh <support@zhelezyaka.kz>
2019-04-25 16:22:31 +0600

    добавляем контейнер ArrayList для хранения наблюдателей и создаем его в конструкторе

---
+++ src/main/java/designPatterns/observer/WeatherData.java
@@ -0,0 +1,29 @@
+package designPatterns.observer;
+
+import java.util.ArrayList;
+
+public class WeatherData implements Subject {
+    private ArrayList observers;
+    private float temperature;
+    private float humidity;
+    private float pressure;
+
+    public WeatherData() {
+        observers = new ArrayList();
+    }
+
+    @Override
+    public void registerObserver(Observer o) {
+
+    }
+
+    @Override
+    public void removeObserver(Observer o) {
+
+    }
+
+    @Override
+    public void notifyObservers() {
+
+    }
+}
