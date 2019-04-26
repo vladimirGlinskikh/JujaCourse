825047e80f0d0433cda3bacce222064815ce1730
VladimirGlinskikh <support@zhelezyaka.kz>
2019-04-26 10:36:08 +0600

    при вызове update() мы сохраняем значения температуры и влажности, после чего вызываем display()

--- src/main/java/designPatterns/observer/CurrentConditionsDisplay.java
+++ src/main/java/designPatterns/observer/CurrentConditionsDisplay.java
@@ -17,7 +17,10 @@ public class CurrentConditionsDisplay implements Observer, DisplayElement {
     }
 
     @Override
-    public void update(float temp, float humidity, float pressure) {
+    public void update(float temperature, float humidity, float pressure) {
+        this.temperature = temperature;
+        this.humidity = humidity;
+        display();
 
     }
 }
