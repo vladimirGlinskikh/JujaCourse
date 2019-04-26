97638db5f82cd832aece4f3dae0a296ddfc0ac8b
VladimirGlinskikh <support@zhelezyaka.kz>
2019-04-26 10:28:01 +0600

    класс CurrentConditionsDisplay реализует Observer и DisplayElement чтобы получать данные от объекта WeatherData.

---
+++ src/main/java/designPatterns/observer/CurrentConditionsDisplay.java
@@ -0,0 +1,13 @@
+package designPatterns.observer;
+
+public class CurrentConditionsDisplay implements Observer, DisplayElement {
+    @Override
+    public void display() {
+        
+    }
+
+    @Override
+    public void update(float temp, float humidity, float pressure) {
+
+    }
+}
