247cd64bb54e2838e0e95b846edaaf75687a5c8e
VladimirGlinskikh <support@zhelezyaka.kz>
2019-04-26 10:31:11 +0600

    конструктору передается объест WeatherData, который используется для регистрации элементов в качестве наблюдателя

--- src/main/java/designPatterns/observer/CurrentConditionsDisplay.java
+++ src/main/java/designPatterns/observer/CurrentConditionsDisplay.java
@@ -1,9 +1,18 @@
 package designPatterns.observer;
 
 public class CurrentConditionsDisplay implements Observer, DisplayElement {
+    private float temperature;
+    private float humidity;
+    private Subject weatherData;
+
+    public CurrentConditionsDisplay(Subject weatherData) {
+        this.weatherData = weatherData;
+        weatherData.registerObserver(this);
+    }
+
     @Override
     public void display() {
-        
+
     }
 
     @Override
