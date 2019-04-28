2d4b07fd6af18bd1b64c31dd29e4e577ed8624f6
Vladimir Glinskikh <support@zhelezyaka.kz>
2019-04-28 11:26:05 +0600

    рефакторим объект WeatherData чтобы он использовал java.util.Observable

--- src/main/java/designPatterns/observer/WeatherData.java
+++ src/main/java/designPatterns/observer/WeatherData.java
@@ -1,36 +1,13 @@
 package designPatterns.observer;
 
-import java.util.ArrayList;
+import java.util.Observable;
 
-public class WeatherData implements Subject {
-    private ArrayList observers;
+public class WeatherData extends Observable {
     private float temperature;
     private float humidity;
     private float pressure;
 
     public WeatherData() {
-        observers = new ArrayList();
-    }
-
-    @Override
-    public void registerObserver(Observer o) {
-        observers.add(o);
-    }
-
-    @Override
-    public void removeObserver(Observer o) {
-        int i = observers.indexOf(o);
-        if (i >= 0) {
-            observers.remove(i);
-        }
-    }
-
-    @Override
-    public void notifyObservers() {
-        for (int i = 0; i < observers.size(); i++) {
-            Observer observer = (Observer) observers.get(i);
-            observer.update(temperature, humidity, pressure);
-        }
     }
 
     public void measurementsChanged() {
