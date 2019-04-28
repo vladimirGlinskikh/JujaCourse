37b02d67a4e102bc28a75ff6a7ca6edeb6cf0451
Vladimir Glinskikh <support@zhelezyaka.kz>
2019-04-28 11:37:24 +0600

    в update() мы сначала проверяем, что субъект относится к типу WeatherData, затем используем его get-методы для получения температуры и влажности, после чего вызываем display()

--- src/main/java/designPatterns/observer/CurrentConditionsDisplay.java
+++ src/main/java/designPatterns/observer/CurrentConditionsDisplay.java
@@ -21,6 +21,11 @@ public class CurrentConditionsDisplay implements Observer, DisplayElement {
 
     @Override
     public void update(Observable obs, Object arg) {
-
+        if (obs instanceof WeatherData) {
+            WeatherData weatherData = (WeatherData) obs;
+            this.temperature = weatherData.getTemperature();
+            this.humidity = weatherData.getHumidity();
+            display();
+        }
     }
 }
