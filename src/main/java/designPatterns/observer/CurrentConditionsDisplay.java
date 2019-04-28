26cb791d1ab9a339e101927ab0e5485031df7313
Vladimir Glinskikh <support@zhelezyaka.kz>
2019-04-28 11:34:29 +0600

    рефакторим CurrentConditionsDisplay

--- src/main/java/designPatterns/observer/CurrentConditionsDisplay.java
+++ src/main/java/designPatterns/observer/CurrentConditionsDisplay.java
@@ -1,13 +1,16 @@
 package designPatterns.observer;
 
+import java.util.Observable;
+import java.util.Observer;
+
 public class CurrentConditionsDisplay implements Observer, DisplayElement {
+    Observable observable;
     private float temperature;
     private float humidity;
-    private Subject weatherData;
 
-    public CurrentConditionsDisplay(Subject weatherData) {
-        this.weatherData = weatherData;
-        weatherData.registerObserver(this);
+    public CurrentConditionsDisplay(Observable observable) {
+        this.observable = observable;
+        observable.addObserver(this);
     }
 
     @Override
@@ -17,10 +20,7 @@ public class CurrentConditionsDisplay implements Observer, DisplayElement {
     }
 
     @Override
-    public void update(float temperature, float humidity, float pressure) {
-        this.temperature = temperature;
-        this.humidity = humidity;
-        display();
+    public void update(Observable obs, Object arg) {
 
     }
 }
