00707cc8612f01cc7565d11233b511d243da0800
VladimirGlinskikh <support@zhelezyaka.kz>
2019-04-30 15:21:39 +0600

    определяем наблюдателя в виде внутреннего класса AngelListener

--- src/main/java/designPatterns/observer/SwingObserverExample.java
+++ src/main/java/designPatterns/observer/SwingObserverExample.java
@@ -1,4 +1,26 @@
 package designPatterns.observer;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class SwingObserverExample {
+    JFrame frame;
+
+    public static void main(String[] args) {
+        SwingObserverExample example = new SwingObserverExample();
+        example.go();
+    }
+
+    public void go() {
+        frame = new JFrame();
+    }
+
+    class AngelListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent event) {
+            System.out.println("Don't do it, you might regret it!");
+        }
+    }
 }
