3996be70a7d2601bb27deede924c10839f324f75
VladimirGlinskikh <support@zhelezyaka.kz>
2019-05-05 01:09:04 +0600

    проверяем работу, пока без JUnit

--- src/main/java/anotherExperiments/one_1/TestAccount.java
+++ src/main/java/anotherExperiments/one_1/TestAccount.java
@@ -3,6 +3,8 @@ package anotherExperiments.one_1;
 public class TestAccount {
     public static void main(String[] args) {
         Account account = new Account(-265.09);
+        account.deposit(265.09);
+        account.withDraw(200.00);
         System.out.println("account balance: " + account.getBalance());
     }
 }
