d24c4cbb1a5a6fac78b4a8dfdbc8cef32eb50f66
VladimirGlinskikh <support@zhelezyaka.kz>
2019-05-05 00:43:07 +0600

    примитивный эксперимент, который показывает состояние счета

---
+++ src/main/java/anotherExperiments/one_1/Account.java
@@ -0,0 +1,9 @@
+package anotherExperiments.one_1;
+
+public class Account {
+    public double balance;
+
+    public Account(double balance) {
+        this.balance = balance;
+    }
+}
---
+++ src/main/java/anotherExperiments/one_1/TestAccount.java
@@ -0,0 +1,8 @@
+package anotherExperiments.one_1;
+
+public class TestAccount {
+    public static void main(String[] args) {
+        Account account = new Account(265.09);
+        System.out.println("account balance: " + account.balance);
+    }
+}
