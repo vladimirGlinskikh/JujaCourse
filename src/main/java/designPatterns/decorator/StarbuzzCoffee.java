880c2dcd9c5c7edb894ef5686698e74f548ddcfe
VladimirGlinskikh <support@zhelezyaka.kz>
2019-05-07 18:41:45 +0600

    Заказываем эспрессо без дополнений, выводим описание и стоимость

--- src/main/java/designPatterns/decorator/StarbuzzCoffee.java
+++ src/main/java/designPatterns/decorator/StarbuzzCoffee.java
@@ -3,5 +3,7 @@ package designPatterns.decorator;
 public class StarbuzzCoffee {
     public static void main(String[] args) {
         Beverage beverage = new Espresso();
+        System.out.println(beverage.getDescription()
+                + " $" + beverage.coast());
     }
 }
