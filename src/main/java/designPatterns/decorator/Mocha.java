5cc9fbb269b19e270ff86e20dca3242b769d80ce
Vladimir Glinskikh <support@zhelezyaka.kz>
2019-05-07 16:04:11 +0600

    класс декоратор Mocha

---
+++ src/main/java/designPatterns/decorator/Mocha.java
@@ -0,0 +1,19 @@
+package designPatterns.decorator;
+
+public class Mocha extends CondimentDecorator {
+    Beverage beverage;
+
+    public Mocha(Beverage beverage) {
+        this.beverage = beverage;
+    }
+
+    @Override
+    public String getDescription() {
+        return beverage.getDescription() + ", Mocha";
+    }
+
+    @Override
+    public double coast() {
+        return 0.20 + beverage.coast();
+    }
+}
