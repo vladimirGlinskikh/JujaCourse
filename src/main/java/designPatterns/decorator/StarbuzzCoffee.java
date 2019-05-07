374ddd501c9e979c0a347e9a5ea89e3048b901c1
VladimirGlinskikh <support@zhelezyaka.kz>
2019-05-07 18:48:56 +0600

    создаем объект DarkRoast, затем заворачиваем в Mocha

--- src/main/java/designPatterns/decorator/StarbuzzCoffee.java
+++ src/main/java/designPatterns/decorator/StarbuzzCoffee.java
@@ -8,5 +8,8 @@ public class StarbuzzCoffee {
 
         Beverage beverage2 = new DarkRoast();
         beverage2 = new Mocha(beverage2);
+        beverage2 = new Mocha(beverage2);
+        System.out.println(beverage2.getDescription()
+                + " $" + beverage2.coast());
     }
 }
