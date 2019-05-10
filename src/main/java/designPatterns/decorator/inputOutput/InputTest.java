51861a10bd1f056e6b72738c9a90b875f03be722
Vladimir Glinskikh <support@zhelezyaka.kz>
2019-05-10 12:05:17 +0600

    изменил путь до файла test.txt

--- src/main/java/designPatterns/decorator/inputOutput/InputTest.java
+++ src/main/java/designPatterns/decorator/inputOutput/InputTest.java
@@ -12,11 +12,11 @@ public class InputTest {
             InputStream in =
                     new LowerCaseInputStream(
                             new BufferedInputStream(
-                                    new FileInputStream("test.txt")
+                                    new FileInputStream("src\\main\\resources\\test.txt")
                             )
                     );
             while ((c = in.read()) >= 0) {
-                System.out.println((char) c);
+                System.out.print((char) c);
             }
             in.close();
         } catch (IOException e) {
