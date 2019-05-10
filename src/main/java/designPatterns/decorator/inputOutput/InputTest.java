d328e78a9de18d6033376df87898efcc8c3a5554
Vladimir Glinskikh <support@zhelezyaka.kz>
2019-05-10 11:54:02 +0600

    создаем объект FileInputStream и декорируем его. Сначала декоратором BufferedInputStream, а затем фильтром LowerCaseInputStream

---
+++ src/main/java/designPatterns/decorator/inputOutput/InputTest.java
@@ -0,0 +1,20 @@
+package designPatterns.decorator.inputOutput;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class InputTest {
+    public static void main(String[] args) throws IOException {
+        int c;
+        try {
+            InputStream in =
+                    new LowerCaseInputStream(
+                            new BufferedInputStream(
+                                    new FileInputStream("test.txt")
+                            )
+                    );
+        }
+    }
+}
