c6f80368435f55649877ab63487afc00cf15cebc
VladimirGlinskikh <support@zhelezyaka.kz>
2019-05-17 03:53:25 +0600

    рефакторим класс. Убираем каст Animal

--- src/main/java/anotherExperiments/collection/comparator/Main.java
+++ src/main/java/anotherExperiments/collection/comparator/Main.java
@@ -1,6 +1,7 @@
 package anotherExperiments.collection.comparator;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,25 +11,45 @@ public class Main {
 
         Animal[] animal = {cat, dog, bird};
 
-        Arrays.sort(animal, new ComparatorByPrice());
+        Arrays.sort(animal, new Comparator<Animal>() {
+            @Override
+            public int compare(Animal o1, Animal o2) {
+                return o1.price - o2.price;
+            }
+        });
         for (Animal tmp : animal) {
             System.out.println(tmp);
         }
         System.out.println("----------------------");
 
-        Arrays.sort(animal, new ComparatorBySpeed());
+        Arrays.sort(animal, new Comparator<Animal>() {
+            @Override
+            public int compare(Animal o1, Animal o2) {
+                return o1.speed - o2.speed;
+            }
+        });
         for (Animal tmp : animal) {
             System.out.println(tmp);
         }
         System.out.println("------------------------");
 
-        Arrays.sort(animal, new ComparatorByColor());
+        Arrays.sort(animal, new Comparator<Animal>() {
+            @Override
+            public int compare(Animal o1, Animal o2) {
+                return o1.breed.compareTo(o2.breed);
+            }
+        });
         for (Animal tmp : animal) {
             System.out.println(tmp);
         }
         System.out.println("-----------------------");
 
-        Arrays.sort(animal, new ComparatorByWeight());
+        Arrays.sort(animal, new Comparator<Animal>() {
+            @Override
+            public int compare(Animal o1, Animal o2) {
+                return o1.weight - o2.weight;
+            }
+        });
         for (Animal tmp : animal) {
             System.out.println(tmp);
         }
