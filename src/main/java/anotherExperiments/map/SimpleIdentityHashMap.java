c6fa74a70665aa2e66f41679ea56cdf87483f571
VladimirGlinskikh <support@zhelezyaka.kz>
2019-05-21 00:47:09 +0600

    пример работы HashMap. IdentityHashMap позволяет дубликат значений, а HashMap не позволяет.

--- src/main/java/anotherExperiments/map/SimpleIdentityHashMap.java
+++ src/main/java/anotherExperiments/map/SimpleIdentityHashMap.java
@@ -1,5 +1,6 @@
 package anotherExperiments.map;
 
+import java.util.HashMap;
 import java.util.IdentityHashMap;
 import java.util.Map;
 import java.util.Set;
@@ -9,12 +10,27 @@ public class SimpleIdentityHashMap {
         IdentityHashMap<Integer, String> identityHashMap = new IdentityHashMap<>();
         identityHashMap.put(new Integer(1), "one");
         identityHashMap.put(new Integer(34), "two");
-        identityHashMap.put(new Integer(19), "one");
+        identityHashMap.put(new Integer(1), "one");
         identityHashMap.put(new Integer(23), "four");
         identityHashMap.put(new Integer(56), "five");
 
         Set set = identityHashMap.entrySet();
         for (Object o : set) {
+            Map.Entry<Integer, String> map = (Map.Entry<Integer, String>) o;
+            System.out.println(map.getKey() + " " + map.getValue());
+        }
+
+        System.out.println("-----------------------------------");
+
+        HashMap<Integer, String> hashMap = new HashMap<>();
+        hashMap.put(new Integer(1), "one");
+        hashMap.put(new Integer(34), "two");
+        hashMap.put(new Integer(1), "one");
+        hashMap.put(new Integer(23), "four");
+        hashMap.put(new Integer(56), "five");
+
+        Set set2 = hashMap.entrySet();
+        for (Object o : set2) {
             Map.Entry<Integer, String> map = (Map.Entry<Integer, String>) o;
             System.out.println(map.getKey() + " " + map.getValue());
         }
