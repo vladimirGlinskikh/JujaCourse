30af2c0a88e5adff459d894478bef69517f3b764
VladimirGlinskikh <support@zhelezyaka.kz>
2019-05-29 01:54:32 +0600

    пример глубокого клонирования через конструктор

--- src/main/java/anotherExperiments/cloning/deepCloningWithConstructor/Main.java
+++ src/main/java/anotherExperiments/cloning/deepCloningWithConstructor/Main.java
@@ -1,4 +1,11 @@
 package anotherExperiments.cloning.deepCloningWithConstructor;
 
 public class Main {
+    public static void main(String[] args) {
+        Student originalStudent = new Student(35, "Vladimir");
+        Student cloneStudent = new Student(originalStudent);
+
+        System.out.println(originalStudent);
+        System.out.println(cloneStudent);
+    }
 }
