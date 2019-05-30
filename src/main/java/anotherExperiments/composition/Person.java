891863808f5b315129a2717609fe27a53466015c
Vladimir Glinskikh <support@zhelezyaka.kz>
2019-05-30 15:35:49 +0600

    примитивный пример композиции

--- src/main/java/anotherExperiments/composition/Person.java
+++ src/main/java/anotherExperiments/composition/Person.java
@@ -1,5 +1,14 @@
 package anotherExperiments.composition;
 
 public class Person {
-    private Job job;
+    private Job job; // composition has-a relationship
+
+    public Person() {
+        this.job = new Job();
+        job.setSalary(2000L);
+    }
+
+    public long getSalary() {
+        return job.getSalary();
+    }
 }
---
+++ src/main/java/anotherExperiments/composition/TestPerson.java
@@ -0,0 +1,9 @@
+package anotherExperiments.composition;
+
+public class TestPerson {
+    public static void main(String[] args) {
+        Person person = new Person();
+        long salary = person.getSalary();
+        System.out.println(salary);
+    }
+}
