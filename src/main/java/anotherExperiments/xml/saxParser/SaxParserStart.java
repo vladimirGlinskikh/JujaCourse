322476fb8bbfe95275d88c092d928704b48d58ae
VladimirGlinskikh <support@zhelezyaka.kz>
2019-06-03 19:20:13 +0600

    пример реализации SAX парсера

--- src/main/java/anotherExperiments/xml/saxParser/SaxParserStart.java
+++ src/main/java/anotherExperiments/xml/saxParser/SaxParserStart.java
@@ -1,37 +1,39 @@
 package anotherExperiments.xml.saxParser;
 
 import org.xml.sax.Attributes;
-import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
-import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 
 public class SaxParserStart {
     public static void main(String[] args) {
-        final String fileName = "/home/vladimir/Documents/IdeaProjects/JujaCourse/src/main/resources/exchange.xml";
+        final String fileName = "/home/vladimir/Documents/IdeaProjects/JujaCourse/src/main/resources/phonebook.xml";
         try {
             SAXParserFactory saxParserFactory = SAXParserFactory.newInstance();
             SAXParser saxParser = saxParserFactory.newSAXParser();
-            DefaultHandler handler = new DefaultHandler();
-        } catch (SAXException | ParserConfigurationException e) {
+            DefaultHandler handler = new DefaultHandler() {
+
+                boolean name = false;
+
+                @Override
+                public void startElement(String uri, String localName, String qName, Attributes attributes) {
+                    if (qName.equalsIgnoreCase("name")) {
+                        name = true;
+                    }
+                }
+
+                @Override
+                public void characters(char ch[], int start, int length) {
+                    if (name) {
+                        System.out.println("Name: " + new String(ch, start, length));
+                        name = false;
+                    }
+                }
+            };
+            saxParser.parse(fileName, handler);
+        } catch (Exception e) {
             e.printStackTrace();
-        }
-    }
-
-    boolean name = false;
-
-    public void StartElement(String uri, String localName, String qName, Attributes attributes) {
-        if (qName.equalsIgnoreCase("name")) {
-            name = true;
-        }
-    }
-
-    public void characters(char ch[], int start, int length) throws SAXException {
-        if (name) {
-            System.out.println("Name: " + new String(ch, start, length));
-            name = false;
         }
     }
 }
