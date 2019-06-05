a653437da692d0b6e0e0277fe32562dd2d493a14
VladimirGlinskikh <support@zhelezyaka.kz>
2019-06-05 11:52:18 +0600

    добавил сырой Лист без дженерика и цикл

--- src/main/java/anotherExperiments/thread/simpleTread/SimpleThread.java
+++ src/main/java/anotherExperiments/thread/simpleTread/SimpleThread.java
@@ -1,10 +1,21 @@
 package anotherExperiments.thread.simpleTread;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SimpleThread extends Thread {
     public static void main(String[] args) {
         System.out.println(Thread.currentThread().getName());
         Thread thread = new Thread(new SimpleThread());
         thread.start();
+
+        List list = new ArrayList();
+        for (int i = 0; i < 10000; i++) {
+            boolean add = list.add(new Object());
+            System.out.println(add);
+        }
+
+        System.out.println("Juja Professional");
     }
 
     @Override
