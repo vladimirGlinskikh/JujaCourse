db4823c73c98626573f509c8b1e2f1752bf818cd
VladimirGlinskikh <support@zhelezyaka.kz>
2019-06-05 14:18:40 +0600

    добавил метки что когда начинается и когда заканчивается

--- src/main/java/anotherExperiments/thread/simpleTread/SimpleThread.java
+++ src/main/java/anotherExperiments/thread/simpleTread/SimpleThread.java
@@ -6,22 +6,28 @@ import java.util.List;
 public class SimpleThread extends Thread {
     public static void main(String[] args) {
         System.out.println(Thread.currentThread().getName());
+        System.out.println("Start Thread main()");
         Thread thread = new Thread(new SimpleThread());
         thread.start();
 
+        System.out.println("true if number is add");
         List list = new ArrayList();
-        for (int i = 0; i < 10000; i++) {
+        System.out.println("go into a cycle");
+        for (int i = 0; i < 10; i++) {
             boolean add = list.add(new Object());
             System.out.println(add);
         }
 
-        System.out.println("Juja Professional");
+        System.out.println("exit from a cycle");
+        System.out.println("Exit from Thread main()");
     }
 
     @Override
     public void run() {
+        System.out.println("Start method run()");
         for (int i = 0; i < 10; i++) {
             System.out.println(Thread.currentThread().getName() + " " + i);
         }
+        System.out.println("End method run()");
     }
 }
