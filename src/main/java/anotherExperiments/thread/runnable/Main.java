5e5818691e87dcbfdfa9df0213c877fee8f2e22c
VladimirGlinskikh <support@zhelezyaka.kz>
2019-06-07 18:48:56 +0600

    пример реализации интерфейса Runnable

---
+++ src/main/java/anotherExperiments/thread/runnable/Main.java
@@ -0,0 +1,34 @@
+package anotherExperiments.thread.runnable;
+
+public class Main implements Runnable {
+    private String word;
+    private int delay;
+
+    private Main(String word, int delay) {
+        this.word = word;
+        this.delay = delay;
+    }
+
+    @Override
+    public void run() {
+        for (; ; ) {
+            System.out.println(word + " ");
+            try {
+                Thread.sleep(delay);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Main ping = new Main("ping", 19);
+        Main pong = new Main("pong", 35);
+
+        Thread thread = new Thread(ping);
+        Thread thread1 = new Thread(pong);
+
+        thread.start();
+        thread1.start();
+    }
+}
