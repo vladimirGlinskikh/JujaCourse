a3ba288d6ce0b69f9d3d404e16a64c3a10c10d60
VladimirGlinskikh <support@zhelezyaka.kz>
2019-06-07 19:01:46 +0600

    написал цикл for и вывел слово яйцо 7 раз

--- src/main/java/anotherExperiments/thread/threads/Main.java
+++ src/main/java/anotherExperiments/thread/threads/Main.java
@@ -6,6 +6,13 @@ public class Main {
 class EggVoice extends Thread {
     @Override
     public void run() {
-
+        for (int i = 0; i < 7; i++) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("яйцо!");
+        }
     }
 }
