68d8ae36f3bc58c67d1721ae4f8836812870d931
VladimirGlinskikh <support@zhelezyaka.kz>
2019-06-10 01:24:54 +0600

    create Row and Cell

--- src/main/java/anotherExperiments/apachePOI/JavaExcel.java
+++ src/main/java/anotherExperiments/apachePOI/JavaExcel.java
@@ -1,6 +1,8 @@
 package anotherExperiments.apachePOI;
 
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 
@@ -11,6 +13,9 @@ public class JavaExcel {
     public static void main(String[] args) {
         Workbook workbook = new HSSFWorkbook();
         Sheet sheet = workbook.createSheet("book");
+        Row row = sheet.createRow(3);
+        Cell cell = row.createCell(5);
+        cell.setCellValue("litres");
         try (FileOutputStream fileOutputStream = new FileOutputStream("/home/vladimir/Documents/IdeaProjects/JujaCourse/src/main/resources/excelBook.xls")) {
             workbook.write(fileOutputStream);
         } catch (IOException e) {
