27a58d3c5b037ec8fa39d5f6bc511f9464f20fc9
VladimirGlinskikh <support@zhelezyaka.kz>
2019-06-10 03:03:45 +0600

    doRefactor. Right getting the cell contents

--- src/main/java/anotherExperiments/apachePOI/read/ReadExcel.java
+++ src/main/java/anotherExperiments/apachePOI/read/ReadExcel.java
@@ -1,6 +1,8 @@
 package anotherExperiments.apachePOI.read;
 
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.DateUtil;
 import org.apache.poi.ss.usermodel.Workbook;
 
 import java.io.FileInputStream;
@@ -10,10 +12,38 @@ public class ReadExcel {
     public static void main(String[] args) {
         try (FileInputStream fileInputStream = new FileInputStream("src/main/resources/excelBook.xls")) {
             Workbook workbook = new HSSFWorkbook(fileInputStream);
-            String result = workbook.getSheetAt(0).getRow(3).getCell(5).getStringCellValue();
-            System.out.println(result);
+//            String result = workbook.getSheetAt(0).getRow(3).getCell(5).getStringCellValue();
+//            System.out.println(result);
+            System.out.println(getCellText(workbook.getSheetAt(0).getRow(3).getCell(5)));
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static String getCellText(Cell cell) {
+
+        String result = "";
+
+        switch (cell.getCellType()) {
+            case STRING:
+                result = cell.getRichStringCellValue().getString();
+                break;
+            case NUMERIC:
+                if (DateUtil.isCellDateFormatted(cell)) {
+                    result = cell.getDateCellValue().toString();
+                } else {
+                    result = Double.toString(cell.getNumericCellValue());
+                }
+                break;
+            case BOOLEAN:
+                result = Boolean.toString(cell.getBooleanCellValue());
+                break;
+            case FORMULA:
+                result = cell.getCellFormula().toString();
+                break;
+            default:
+                break;
+        }
+        return result;
     }
 }
--- src/main/java/anotherExperiments/apachePOI/write/JavaExcel.java
+++ src/main/java/anotherExperiments/apachePOI/write/JavaExcel.java
@@ -15,7 +15,7 @@ public class JavaExcel {
         Sheet sheet = workbook.createSheet("book");
         Row row = sheet.createRow(3);
         Cell cell = row.createCell(5);
-        cell.setCellValue("litres");
+        cell.setCellValue(45.23);
         try (FileOutputStream fileOutputStream = new FileOutputStream("/home/vladimir/Documents/IdeaProjects/JujaCourse/src/main/resources/excelBook.xls")) {
             workbook.write(fileOutputStream);
         } catch (IOException e) {
