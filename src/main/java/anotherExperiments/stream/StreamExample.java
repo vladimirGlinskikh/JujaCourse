aeb8b89ab0c0fdab8241697492f2fb220eaaf3b4
VladimirGlinskikh <support@zhelezyaka.kz>
2019-06-14 22:58:42 +0600

    method peek() is working with jdk_11 if use terminal operation collect()

--- src/main/java/anotherExperiments/stream/StreamExample.java
+++ src/main/java/anotherExperiments/stream/StreamExample.java
@@ -2,12 +2,14 @@ package anotherExperiments.stream;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StreamExample {
     public static void main(String[] args) {
         List<String> list = Arrays.asList("A", "B", "C", "D", "E");
-        long count = list.stream()
-                .peek(System.out::println).count();
-        System.out.println(count);
+        List<String> count = list.stream()
+                .peek(System.out::println)
+                .collect(Collectors.toList());
+        System.out.println(count.size());
     }
 }
