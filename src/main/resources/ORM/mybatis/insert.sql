47c3e10c746443db32e5d6119eca6d08bb02db8c
Vladimir Glinskikh <support@zhelezyaka.kz>
2019-06-19 18:20:31 +0600

    add create.sql and insert.sql

---
+++ src/main/resources/ORM/mybatis/insert.sql
@@ -0,0 +1,10 @@
+INSERT into pet(id, name, owner, species, sex, birth, death)
+values (null, 'Vovo', 'own', 'cpec', 'm', '23-12-02', '23-12-12');
+INSERT into pet(id, name, owner, species, sex, birth, death)
+values (null, 'Abovo', 'own', 'cpec', 'f', '23-12-02', '23-12-12');
+INSERT into pet(id, name, owner, species, sex, birth, death)
+values (null, 'Colvo', 'own', 'cpec', 'f', '23-12-02', '23-12-12');
+INSERT into pet(id, name, owner, species, sex, birth, death)
+values (null, 'Molvo', 'own', 'cpec', 'm', '23-12-02', '23-12-12');
+INSERT into pet(id, name, owner, species, sex, birth, death)
+values (null, 'Glovo', 'own', 'cpec', 'f', '23-12-02', '23-12-12');
