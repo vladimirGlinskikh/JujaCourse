acea957115a89398dc40f9d96c736b03e3437c45
Vladimir Glinskikh <support@zhelezyaka.kz>
2019-06-19 14:44:03 +0600

    create class PetDVO

---
+++ src/main/java/anotherExperiments/ORM/mybatis/db/PetDVO.java
@@ -0,0 +1,70 @@
+package anotherExperiments.ORM.mybatis.db;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class PetDVO implements Serializable {
+    private Integer id;
+    private String name;
+    private String owner;
+    private String species;
+    private String sex;
+    private Date birth;
+    private Date death;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public Date getBirth() {
+        return birth;
+    }
+
+    public void setBirth(Date birth) {
+        this.birth = birth;
+    }
+
+    public Date getDeath() {
+        return death;
+    }
+
+    public void setDeath(Date death) {
+        this.death = death;
+    }
+}
