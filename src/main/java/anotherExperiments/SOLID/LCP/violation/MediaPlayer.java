3cfe1f36c22b351ad7964b64b88de2773560551b
VladimirGlinskikh <support@zhelezyaka.kz>
2019-06-25 21:59:51 +0600

    create violation principle LCP

---
+++ src/main/java/anotherExperiments/SOLID/LCP/violation/MediaPlayer.java
@@ -0,0 +1,11 @@
+package anotherExperiments.SOLID.LCP.violation;
+
+public class MediaPlayer {
+    public void playAudio() {
+        System.out.println("Playing something audio...");
+    }
+
+    public void playVideo() {
+        System.out.println("Playing something video...");
+    }
+}
