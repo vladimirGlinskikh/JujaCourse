1299f5d7f1161371e3d827f63d23ed02e327e1a4
VladimirGlinskikh <support@zhelezyaka.kz>
2019-06-29 22:43:14 +0600

    example annotation @ComponentScan

---
+++ src/main/java/springFramework/ch3/annotation/HelloJujaConfiguration.java
@@ -0,0 +1,9 @@
+package springFramework.ch3.annotation;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan(basePackages = "springFramework.ch3.annotation")
+@Configuration
+public class HelloJujaConfiguration {
+}
