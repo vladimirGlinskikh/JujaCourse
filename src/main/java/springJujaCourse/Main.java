b57e60ea7111d0ad6d89360bd76500fc3b1c8282
VladimirGlinskikh <support@zhelezyaka.kz>
2019-06-30 22:02:28 +0600

    create another bean

--- src/main/java/springJujaCourse/Main.java
+++ src/main/java/springJujaCourse/Main.java
@@ -6,7 +6,10 @@ public class Main {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext(new String[] {"springJuja/config.xml"});
         MyBean beans = (MyBean) ac.getBean("beans");
+        MyBean one = (MyBean) ac.getBean("qwe");
         beans.sayHello();
+        one.sayHello();
         System.out.println(beans.getName());
+        System.out.println(one.getName());
     }
 }
