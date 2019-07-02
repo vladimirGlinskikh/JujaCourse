8d29db0b42114bef7896f7903472f014a30fa353
VladimirGlinskikh <support@zhelezyaka.kz>
2019-07-02 22:21:42 +0600

    init and destroy methods

--- src/main/java/springJujaCourse/springCourse/ClassicalMusic.java
+++ src/main/java/springJujaCourse/springCourse/ClassicalMusic.java
@@ -1,6 +1,14 @@
 package springJujaCourse.springCourse;
 
 public class ClassicalMusic implements Music {
+    public void doMyInit() {
+        System.out.println("Doing my initialization...");
+    }
+
+    public void doMyDestroy() {
+        System.out.println("Doing my destroy...");
+    }
+
     @Override
     public String getSong() {
         return "Something classic music...";
