d6690469f8937d2ea3fed4882297c79f758728ec
VladimirGlinskikh <support@zhelezyaka.kz>
2019-07-02 22:36:48 +0600

    add fabric method pattern

--- src/main/java/springJujaCourse/springCourse/ClassicalMusic.java
+++ src/main/java/springJujaCourse/springCourse/ClassicalMusic.java
@@ -1,6 +1,13 @@
 package springJujaCourse.springCourse;
 
 public class ClassicalMusic implements Music {
+    private ClassicalMusic() {
+    }
+
+    public static ClassicalMusic getClassicalMusic() {
+        return new ClassicalMusic();
+    }
+
     public void doMyInit() {
         System.out.println("Doing my initialization...");
     }
