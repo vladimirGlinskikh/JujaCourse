bddedd61fbc273e1429748942f583b2fb5cc6761
VladimirGlinskikh <support@zhelezyaka.kz>
2019-07-04 18:32:48 +0600

    measurements insertion

---
+++ src/main/java/anotherExperiments/collection/list/ListTest.java
@@ -0,0 +1,25 @@
+package anotherExperiments.collection.list;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ListTest {
+    public static void main(String[] args) {
+        List<Integer> linkedList = new LinkedList<>();
+        List<Integer> arrayList = new ArrayList<>();
+
+        measureTime(linkedList);
+        measureTime(arrayList);
+
+    }
+
+    private static void measureTime(List<Integer> list) {
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 1_000_000; i++) {
+            list.add(i);
+        }
+        long end = System.currentTimeMillis();
+        System.out.println(end - start);
+    }
+}
