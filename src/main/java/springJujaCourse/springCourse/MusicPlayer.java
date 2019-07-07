cd55a6b9ba4f199005cfa2c824f149c726399405
VladimirGlinskikh <support@zhelezyaka.kz>
2019-07-07 21:52:04 +0600

    inject across fields

--- src/main/java/springJujaCourse/springCourse/MusicPlayer.java
+++ src/main/java/springJujaCourse/springCourse/MusicPlayer.java
@@ -6,16 +6,17 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
+    @Autowired
     private Music music;
 
-    public MusicPlayer(Music music) {
-        this.music = music;
-    }
+//    public MusicPlayer(Music music) {
+//        this.music = music;
+//    }
 
-    @Autowired
-    public void setMusic(Music music) {
-        this.music = music;
-    }
+//    @Autowired
+//    public void setMusic(Music music) {
+//        this.music = music;
+//    }
 
     public void playMusic() {
         System.out.println("Playing: " + music.getSong());
