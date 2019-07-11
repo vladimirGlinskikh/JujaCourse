307115ab853c6a5c452834a9ce53d78f8ac2b5f5
VladimirGlinskikh <support@zhelezyaka.kz>
2019-07-11 22:51:13 +0600

    sort with CompareTo() by id

---
+++ src/main/java/anotherExperiments/comparable/Person.java
@@ -0,0 +1,50 @@
+package anotherExperiments.comparable;
+
+import java.util.Objects;
+
+public class Person implements Comparable<Person> {
+    private int id;
+    private String name;
+
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return id == person.id &&
+                Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Person o) {
+        if (this.id > o.getId()) {
+            return 1;
+        } else if (this.id < o.getId()) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+}
---
+++ src/main/java/anotherExperiments/comparable/TestPerson.java
@@ -0,0 +1,25 @@
+package anotherExperiments.comparable;
+
+import java.util.*;
+
+public class TestPerson {
+    public static void main(String[] args) {
+        List<Person> peopleList = new ArrayList<>();
+        Set<Person> personSet = new TreeSet<>();
+
+        addElements(peopleList);
+        addElements(personSet);
+
+        Collections.sort(peopleList);
+
+        System.out.println(peopleList);
+        System.out.println(personSet);
+    }
+
+    private static void addElements(Collection collection) {
+        collection.add(new Person(1, "Vladimir"));
+        collection.add(new Person(3, "Vlad"));
+        collection.add(new Person(2, "Geny"));
+
+    }
+}
