457ae7434c95a9aa624613595089c9fcc0407651
VladimirGlinskikh <support@zhelezyaka.kz>
2019-07-11 13:06:35 +0600

    sorting number by descending

--- src/main/java/anotherExperiments/comparator/BackwardsIntegerComparator.java
+++ src/main/java/anotherExperiments/comparator/BackwardsIntegerComparator.java
@@ -6,9 +6,9 @@ public class BackwardsIntegerComparator implements Comparator<Integer> {
     @Override
     public int compare(Integer o1, Integer o2) {
         if (o1 > o2) {
-            return 1;
-        } else if (o1 < o2) {
             return -1;
+        } else if (o1 < o2) {
+            return 1;
         } else {
             return 0;
         }
