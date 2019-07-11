a7b9188a5bc2b1b258fedb149eb1050ae1131f9e
VladimirGlinskikh <support@zhelezyaka.kz>
2019-07-11 12:05:15 +0600

    standard sorting numbers

--- src/main/java/anotherExperiments/comparator/Main.java
+++ src/main/java/anotherExperiments/comparator/Main.java
@@ -14,5 +14,16 @@ public class Main {
 
         Collections.sort(list);
         System.out.println(list);
+
+        List<Integer> numbers = new ArrayList<>();
+
+        numbers.add(1);
+        numbers.add(4);
+        numbers.add(3);
+        numbers.add(9);
+        numbers.add(2);
+
+        Collections.sort(numbers);
+        System.out.println(numbers);
     }
 }
