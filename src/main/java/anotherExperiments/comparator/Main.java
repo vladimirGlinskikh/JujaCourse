d9b0fe39428f9f88e05f67e27b8df6b8a17364fc
VladimirGlinskikh <support@zhelezyaka.kz>
2019-07-11 12:55:39 +0600

    sorting by length

--- src/main/java/anotherExperiments/comparator/Main.java
+++ src/main/java/anotherExperiments/comparator/Main.java
@@ -11,19 +11,20 @@ public class Main {
         list.add("bird");
         list.add("fish");
         list.add("animals");
+        list.add("an");
 
-        Collections.sort(list);
+        Collections.sort(list, new StringLengthComparator());
         System.out.println(list);
 
-        List<Integer> numbers = new ArrayList<>();
-
-        numbers.add(1);
-        numbers.add(4);
-        numbers.add(3);
-        numbers.add(9);
-        numbers.add(2);
-
-        Collections.sort(numbers);
-        System.out.println(numbers);
+//        List<Integer> numbers = new ArrayList<>();
+//
+//        numbers.add(1);
+//        numbers.add(4);
+//        numbers.add(3);
+//        numbers.add(9);
+//        numbers.add(2);
+//
+//        Collections.sort(numbers);
+//        System.out.println(numbers);
     }
 }
