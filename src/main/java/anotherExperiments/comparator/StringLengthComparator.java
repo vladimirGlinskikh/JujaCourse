deb338f43127913cf5aa263e33c7c0aff9eb5647
VladimirGlinskikh <support@zhelezyaka.kz>
2019-07-11 12:47:58 +0600

    realization method compare()

---
+++ src/main/java/anotherExperiments/comparator/StringLengthComparator.java
@@ -0,0 +1,17 @@
+package anotherExperiments.comparator;
+
+import java.util.Comparator;
+
+public class StringLengthComparator implements Comparator<String> {
+
+    @Override
+    public int compare(String o1, String o2) {
+        if (o1.length() > o2.length()) {
+            return 1;
+        } else if (o1.length() < o2.length()) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+}
