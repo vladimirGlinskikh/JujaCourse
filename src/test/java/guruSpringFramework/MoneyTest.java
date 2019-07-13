0713f8980e021daef77cb1e27735a03c3627b90f
VladimirGlinskikh <support@zhelezyaka.kz>
2019-07-13 21:46:59 +0600

    we get green line test. Privacy.

--- src/test/java/guruSpringFramework/MoneyTest.java
+++ src/test/java/guruSpringFramework/MoneyTest.java
@@ -3,16 +3,21 @@ package guruSpringFramework;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class MoneyTest {
     @Test
     void testMultiplication() {
         Dollar five = new Dollar(5);
         Dollar product = five.times(2);
-        assertEquals(10, product.amount);
+        assertEquals(new Dollar(10), product);
         product = five.times(3);
-        assertEquals(15, product.amount);
-        product = five.times(5);
-        assertEquals(25, product.amount);
+        assertEquals(new Dollar(15), product);
+    }
+
+    @Test
+    void testEquality() {
+        assertEquals(new Dollar(5), new Dollar(5));
+        assertNotEquals(new Dollar(5), new Dollar(8));
     }
 }
