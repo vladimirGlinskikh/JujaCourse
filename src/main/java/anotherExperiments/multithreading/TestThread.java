25fc07925e76aebe9fe99815ec874b2ceb488c64
VladimirGlinskikh <support@zhelezyaka.kz>
2019-07-16 00:16:08 +0600

    primitive example method sleep()

--- src/main/java/anotherExperiments/multithreading/TestThread.java
+++ src/main/java/anotherExperiments/multithreading/TestThread.java
@@ -3,10 +3,18 @@ package anotherExperiments.multithreading;
 public class TestThread {
     public static void main(String[] args) {
 
-        MyThread thread1 = new MyThread();
-        thread1.start();
-        MyThread thread2 = new MyThread();
-        thread2.start();
+//        MyThread thread1 = new MyThread();
+//        thread1.start();
+//        MyThread thread2 = new MyThread();
+//        thread2.start();
+
+        System.out.println("I'm going to sleep");
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("I am awake!");
     }
 }
 
