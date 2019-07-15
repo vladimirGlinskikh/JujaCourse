23d745bf0f9e68a3057670ec734bc1c2c0c21b28
VladimirGlinskikh <support@zhelezyaka.kz>
2019-07-15 13:00:30 +0600

    doRefactor. Deleted method equals() in classes Dollar and Frank

--- src/main/java/guruSpringFramework/Dollar.java
+++ src/main/java/guruSpringFramework/Dollar.java
@@ -9,9 +9,4 @@ public class Dollar extends Money {
     public Dollar times(int multiplier) {
         return new Dollar(amount * multiplier);
     }
-
-    public boolean equals(Object object) {
-        Dollar dollar = (Dollar) object;
-        return amount == dollar.amount;
-    }
 }
--- src/main/java/guruSpringFramework/Frank.java
+++ src/main/java/guruSpringFramework/Frank.java
@@ -10,9 +10,4 @@ public class Frank extends Money {
     public Frank times(int multiplier) {
         return new Frank(amount * multiplier);
     }
-
-    public boolean equals(Object object) {
-        Frank frank = (Frank) object;
-        return amount == frank.amount;
-    }
 }
