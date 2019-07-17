d0231744607db22caf5be154bdbfbebb71496f40
VladimirGlinskikh <support@zhelezyaka.kz>
2019-07-17 21:08:47 +0600

    example is two not synchronized threads

--- src/main/java/anotherExperiments/multithreading/TestSynchronized.java
+++ src/main/java/anotherExperiments/multithreading/TestSynchronized.java
@@ -4,35 +4,6 @@ public class TestSynchronized {
     private int counter;
 
     public static void main(String[] args) throws InterruptedException {
-        TestSynchronized test = new TestSynchronized();
-        test.doWork();
-    }
-
-    private void increment() {
-        synchronized (this) {
-            counter++;
-        }
-    }
-
-    public void doWork() throws InterruptedException {
-        Thread thread1 = new Thread(() -> {
-            for (int i = 0; i < 100000; i++) {
-                increment();
-            }
-        });
-
-        Thread thread2 = new Thread(() -> {
-            for (int i = 0; i < 100000; i++) {
-                increment();
-            }
-        });
-
-        thread1.start();
-        thread2.start();
-
-        thread1.join();
-        thread2.join();
-
-        System.out.println(counter);
+        new Worker().main();
     }
 }
---
+++ src/main/java/anotherExperiments/multithreading/Worker.java
@@ -0,0 +1,47 @@
+package anotherExperiments.multithreading;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Worker {
+    Random random = new Random();
+
+    private List<Integer> list1 = new ArrayList<>();
+    private List<Integer> list2 = new ArrayList<>();
+
+    public void addToListOne() {
+        try {
+            Thread.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        list1.add(random.nextInt(100));
+    }
+
+    public void addToListTwo() {
+        try {
+            Thread.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        list2.add(random.nextInt(100));
+    }
+
+    public void work() {
+        for (int i = 0; i < 1000; i++) {
+            addToListOne();
+            addToListTwo();
+        }
+    }
+
+    public void main() {
+        long before = System.currentTimeMillis();
+        work();
+        long after = System.currentTimeMillis();
+        System.out.println("Program took for: " + (after - before));
+
+        System.out.println("List1: " + list1.size());
+        System.out.println("List2: " + list2.size());
+    }
+}
