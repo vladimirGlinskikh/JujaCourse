df9608bd368dac532aa51a77b87855c93e619937
VladimirGlinskikh <support@zhelezyaka.kz>
2019-07-19 23:52:00 +0600

    example working CountDownLatch

---
+++ src/main/java/anotherExperiments/multithreading/countDownLatch/Processor.java
@@ -0,0 +1,21 @@
+package anotherExperiments.multithreading.countDownLatch;
+
+import java.util.concurrent.CountDownLatch;
+
+public class Processor implements Runnable {
+    private CountDownLatch countDownLatch;
+
+    public Processor(CountDownLatch countDownLatch) {
+        this.countDownLatch = countDownLatch;
+    }
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        countDownLatch.countDown();
+    }
+}
---
+++ src/main/java/anotherExperiments/multithreading/countDownLatch/TestCountDownLatch.java
@@ -0,0 +1,20 @@
+package anotherExperiments.multithreading.countDownLatch;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class TestCountDownLatch {
+    public static void main(String[] args) throws InterruptedException {
+        CountDownLatch countDownLatch = new CountDownLatch(3);
+
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+
+        for (int i = 0; i < 3; i++)
+            executorService.submit(new Processor(countDownLatch));
+        executorService.shutdown();
+        countDownLatch.await();
+        System.out.println("Latch has been opened, main thread is proceeding!");
+
+    }
+}
