5709a3a721f72abebf6da824f291ca9b6843e36f
VladimirGlinskikh <support@zhelezyaka.kz>
2019-07-23 21:11:03 +0600

    At the moment we see that class ReentrantLock work and out correct result.

--- src/main/java/anotherExperiments/multithreading/reentrantLock/Task.java
+++ src/main/java/anotherExperiments/multithreading/reentrantLock/Task.java
@@ -1,7 +1,11 @@
 package anotherExperiments.multithreading.reentrantLock;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Task {
     private int counter;
+    private Lock lock = new ReentrantLock();
 
     private void increment() {
         for (int i = 0; i < 10_000; i++) {
@@ -10,11 +14,15 @@ public class Task {
     }
 
     public void firstThread() {
+        lock.lock();
         increment();
+        lock.unlock();
     }
 
     public void secondThread() {
+        lock.lock();
         increment();
+        lock.unlock();
     }
 
     public void showCounter() {
