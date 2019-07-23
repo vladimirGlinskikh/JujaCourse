b75414a84c790d88760cf5f562b51f48aa56338e
VladimirGlinskikh <support@zhelezyaka.kz>
2019-07-23 23:34:29 +0600

    example with fix deadlock

--- src/main/java/anotherExperiments/multithreading/deadLock/Runner.java
+++ src/main/java/anotherExperiments/multithreading/deadLock/Runner.java
@@ -11,12 +11,40 @@ public class Runner {
     private Lock lock1 = new ReentrantLock();
     private Lock lock2 = new ReentrantLock();
 
+    private void takeLocks(Lock lock1, Lock lock2) {
+        boolean firsLockTaken = false;
+        boolean secondLockTaken = false;
+
+        while (true) {
+            try {
+                firsLockTaken = lock1.tryLock();
+                secondLockTaken = lock2.tryLock();
+            } finally {
+                if (firsLockTaken && secondLockTaken) {
+                    return;
+                }
+                if (firsLockTaken) {
+                    lock1.unlock();
+                }
+
+                if (secondLockTaken) {
+                    lock2.unlock();
+                }
+            }
+
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
     public void firstThread() {
         Random random = new Random();
 
         for (int i = 0; i < 10_000; i++) {
-            lock1.lock();
-            lock2.lock();
+            takeLocks(lock1, lock2);
             try {
                 Account.transfer(account1, account2, random.nextInt(100));
             } finally {
@@ -30,8 +58,7 @@ public class Runner {
         Random random = new Random();
 
         for (int i = 0; i < 10_000; i++) {
-            lock2.lock();
-            lock1.lock();
+            takeLocks(lock2, lock1);
             try {
                 Account.transfer(account2, account1, random.nextInt(100));
             } finally {
