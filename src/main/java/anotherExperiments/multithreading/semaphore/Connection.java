242d98ad7731cdf9ee600230ae4718f78698d1b4
VladimirGlinskikh <support@zhelezyaka.kz>
2019-07-23 21:31:31 +0600

    create class Connection that simulation connect with server

---
+++ src/main/java/anotherExperiments/multithreading/semaphore/Connection.java
@@ -0,0 +1,4 @@
+package anotherExperiments.multithreading.semaphore;
+
+public class Connection {
+}
--- src/main/java/anotherExperiments/multithreading/semaphore/Test.java
+++ src/main/java/anotherExperiments/multithreading/semaphore/Test.java
@@ -4,21 +4,8 @@ import java.util.concurrent.Semaphore;
 
 public class Test {
     public static void main(String[] args) {
-        Semaphore semaphore = new Semaphore(3);
 
-        try {
-            semaphore.acquire();
-            semaphore.acquire();
-            semaphore.acquire();
 
-            System.out.println("All permits have been acquired!");
-            semaphore.acquire();
-            System.out.println("I Can't reach here...");
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        semaphore.release();
 
-        System.out.println(semaphore.availablePermits());
     }
 }
