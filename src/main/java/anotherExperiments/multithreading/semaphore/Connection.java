cd34e88804d8305484dc434d238f96dab5dceb17
VladimirGlinskikh <support@zhelezyaka.kz>
2019-07-23 21:49:30 +0600

    example work Semaphore

--- src/main/java/anotherExperiments/multithreading/semaphore/Connection.java
+++ src/main/java/anotherExperiments/multithreading/semaphore/Connection.java
@@ -1,4 +1,37 @@
 package anotherExperiments.multithreading.semaphore;
 
+import java.util.concurrent.Semaphore;
+
 public class Connection {
+    private static Connection connection = new Connection();
+    private int connectionsCount;
+    private Semaphore semaphore = new Semaphore(10);
+
+    private Connection() {
+
+    }
+
+    public static Connection getConnection() {
+        return connection;
+    }
+
+    public void work() throws InterruptedException {
+        semaphore.acquire();
+        try {
+            doWork();
+        } finally {
+            semaphore.release();
+        }
+    }
+
+    private void doWork() throws InterruptedException {
+        synchronized (this) {
+            connectionsCount++;
+            System.out.println(connectionsCount);
+        }
+        Thread.sleep(5000);
+        synchronized (this) {
+            connectionsCount--;
+        }
+    }
 }
--- src/main/java/anotherExperiments/multithreading/semaphore/Test.java
+++ src/main/java/anotherExperiments/multithreading/semaphore/Test.java
@@ -1,11 +1,28 @@
 package anotherExperiments.multithreading.semaphore;
 
-import java.util.concurrent.Semaphore;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(200);
+        Connection connection = Connection.getConnection();
 
+        for (int i = 0; i < 200; i++) {
+            executorService.submit(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        connection.work();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
 
-
+        executorService.shutdown();
+        executorService.awaitTermination(1, TimeUnit.DAYS);
     }
 }
