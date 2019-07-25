4b0eb6a68c2d9502b9ec40508d06eca6c45a2043
VladimirGlinskikh <support@zhelezyaka.kz>
2019-07-25 23:14:00 +0600

    regex example

--- src/main/java/anotherExperiments/regex/Test.java
+++ src/main/java/anotherExperiments/regex/Test.java
@@ -2,7 +2,22 @@ package anotherExperiments.regex;
 
 public class Test {
     public static void main(String[] args) {
-        String string = "12345";
-        System.out.println(string.matches("1234"));
+        /*
+        \\d - одна цифра
+        + - 1 или более
+        * - 0 или более
+        ? - 0 или 1 символов до
+
+        ( | | ) - одна строка из множества строк
+        [a-zA-Z] - все английские буквы
+        [0-9] - все цифры
+        [^abc] - мы хотим все символы кроме [abc]
+        . - абсолютно любой символ
+        {3} - 3 символа до
+        {3, 7} - от 3 до 7 символов
+        \\w - одна буква
+         */
+        String string = "43";
+        System.out.println(string.matches("\\d+"));
     }
 }
