b2dbcd19745739d7114b6d6b03fad1ffff8b059e
VladimirGlinskikh <support@zhelezyaka.kz>
2019-07-25 22:44:34 +0600

    method matches()

---
+++ src/main/java/anotherExperiments/regex/Test.java
@@ -0,0 +1,8 @@
+package anotherExperiments.regex;
+
+public class Test {
+    public static void main(String[] args) {
+        String string = "12345";
+        System.out.println(string.matches("1234"));
+    }
+}
