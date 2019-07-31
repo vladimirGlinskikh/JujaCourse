9551c9acba3cd54de66167cbdcc56afefde30ccc
VladimirGlinskikh <support@zhelezyaka.kz>
2019-07-31 23:04:38 +0600

    introducing simple values

---
+++ src/main/java/springFramework/ch3/xml/InjectSimple.java
@@ -0,0 +1,50 @@
+package springFramework.ch3.xml;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class InjectSimple {
+    private String name;
+    private int age;
+    private float height;
+    private boolean programmer;
+    private Long ageInSeconds;
+
+    public static void main(String[] args) {
+        GenericXmlApplicationContext context =
+                new GenericXmlApplicationContext();
+        context.load("classpath:spring/app-context-xml.xml");
+        context.refresh();
+        InjectSimple simple = (InjectSimple) context.getBean("injectSimple");
+        System.out.println(simple);
+        context.close();
+    }
+
+    public void setProgrammer(boolean programmer) {
+        this.programmer = programmer;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAgeInSeconds(Long ageInSeconds) {
+        this.ageInSeconds = ageInSeconds;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + name + "\n"
+                + "Age: " + age + "\n"
+                + "Age in Seconds: " + ageInSeconds + "\n"
+                + "Height: " + height + "\n"
+                + "Is Programmer?: " + programmer;
+    }
+}
