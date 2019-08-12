6c85a494e690bd71b79f15e3f8ba334a8b250eea
VladimirGlinskikh <support@zhelezyaka.kz>
2019-08-12 23:35:17 +0600

    in this example, we see how the alias works

---
+++ src/main/java/springFramework/ch3/config/AliasConfigDemo.java
@@ -0,0 +1,36 @@
+package springFramework.ch3.config;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import springFramework.ch3.annotated.Singer;
+
+import java.util.Arrays;
+import java.util.Map;
+
+public class AliasConfigDemo {
+
+    @Configuration
+    public static class AliasBeanConfig {
+
+        @Bean
+        public Singer singer() {
+            return new Singer();
+        }
+    }
+
+    public static void main(String[] args) {
+        GenericApplicationContext ctx = new AnnotationConfigApplicationContext(AliasBeanConfig.class);
+
+        Map<String, Singer> beans = ctx.getBeansOfType(Singer.class);
+        beans.entrySet().stream()
+                .forEach(b -> System.out.println("id: "
+                        + b.getKey()
+                        + "\n aliases: "
+                        + Arrays.toString(ctx.getAliases(b.getKey()))
+                        + "\n"));
+        ctx.close();
+    }
+}
