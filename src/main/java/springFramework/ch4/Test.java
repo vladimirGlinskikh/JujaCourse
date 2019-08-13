405e132d6c4a19cd704080a6e20e974fd66fcf8b
VladimirGlinskikh <support@zhelezyaka.kz>
2019-08-13 21:16:43 +0600

    question from the interview

---
+++ src/main/java/springFramework/ch4/Test.java
@@ -0,0 +1,14 @@
+package springFramework.ch4;
+
+public class Test {
+    public static void main(String[] args) {
+        for (int a = 0; a < 10; a++) {
+            System.out.println(a);
+            /*
+            Вопрос с собеседования: Чему будет равна переменная а после выполнения кода?
+            Ответ: переменная а будет равна 10, а на экран выведет число 9, так-как в Java
+            отсчет начинается с 0.
+             */
+        }
+    }
+}
