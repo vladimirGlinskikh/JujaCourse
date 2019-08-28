0307e6b7a4c2e25508487e5fe6c32fcd1c4dca51
VladimirGlinskikh <support@zhelezyaka.kz>
2019-08-29 01:33:18 +0600

    create init() doGet() and signUp.jsp

--- src/main/java/jspExamples/kz/zhelezyaka/servlets/SignUpServlet.java
+++ src/main/java/jspExamples/kz/zhelezyaka/servlets/SignUpServlet.java
@@ -1,5 +1,10 @@
 package jspExamples.kz.zhelezyaka.servlets;
 
+import jspExamples.kz.zhelezyaka.models.User;
+import jspExamples.kz.zhelezyaka.repositories.UsersRepository;
+import jspExamples.kz.zhelezyaka.repositories.UsersRepositoryInMemoryImpl;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,12 +12,22 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 @WebServlet("/signUp")
 public class SignUpServlet extends HttpServlet {
+    private UsersRepository usersRepository;
+
+    @Override
+    public void init() throws ServletException {
+        this.usersRepository = new UsersRepositoryInMemoryImpl();
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter writer = resp.getWriter();
-        writer.write("Hello Juja!");
+        List<User> users = usersRepository.findAll();
+        req.setAttribute("usersFromServer", users);
+        RequestDispatcher dispatcher = req.getServletContext().getRequestDispatcher("/JSP/signUp.jsp");
+        dispatcher.forward(req, resp);
     }
 }
