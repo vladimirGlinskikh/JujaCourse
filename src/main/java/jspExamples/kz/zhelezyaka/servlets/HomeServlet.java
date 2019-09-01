a5968ff71d42166e5c7b75a660a664ab6984c314
VladimirGlinskikh <support@zhelezyaka.kz>
2019-09-01 23:04:49 +0600

    just playing with cookies

--- src/main/java/jspExamples/kz/zhelezyaka/servlets/HomeServlet.java
+++ src/main/java/jspExamples/kz/zhelezyaka/servlets/HomeServlet.java
@@ -2,6 +2,7 @@ package jspExamples.kz.zhelezyaka.servlets;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,5 +13,13 @@ public class HomeServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.getServletContext().getRequestDispatcher("/WEB-INF/JSP/home.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String color = req.getParameter("color");
+
+        Cookie colorCookie = new Cookie("color", color);
+        resp.addCookie(colorCookie);
     }
 }
--- src/main/webapp/WEB-INF/JSP/home.jsp
+++ src/main/webapp/WEB-INF/JSP/home.jsp
@@ -4,13 +4,13 @@
     <title>Title</title>
 </head>
 <body>
-<span style="color: #00CC00">Hello Juja!</span>
-<form>
+<span style="color: ${cookie.color.value}">Hello Juja!</span>
+<form method="post" action="/JujaCourse_war_exploded/home">
     <label for="color">
         <select name="color" id="color">
-            <option name="blue">This blue color</option>
-            <option name="black">This black color</option>
-            <option name="red">This red color</option>
+            <option value="blue">This blue color</option>
+            <option value="black">This black color</option>
+            <option value="red">This red color</option>
         </select>
     </label>
     <input type="submit" value="Color send">
