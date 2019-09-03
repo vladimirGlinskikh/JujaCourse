8dc989b64fd08f27bc6f85b161470cc591a3d7a8
VladimirGlinskikh <support@zhelezyaka.kz>
2019-09-03 21:21:21 +0600

    connect to dataBase

--- src/main/java/dataBaseExample/kz/zhelezyaka/servlets/UsersServlet.java
+++ src/main/java/dataBaseExample/kz/zhelezyaka/servlets/UsersServlet.java
@@ -1,12 +1,18 @@
 package dataBaseExample.kz.zhelezyaka.servlets;
 
+import org.postgresql.Driver;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
 @WebServlet("/users")
 public class UsersServlet extends HttpServlet {
@@ -14,8 +20,24 @@ public class UsersServlet extends HttpServlet {
 
 
     @Override
-    public void init() throws ServletException {
+    public void init() {
+        Properties properties = new Properties();
+        try {
+            properties.load(new FileInputStream(getServletContext().getRealPath("/WEB-INF/classes/db.properties")));
+            String dbUrl = properties.getProperty("db.url");
+            String dbUsername = properties.getProperty("db.username");
+            String dbPassword = properties.getProperty("db.password");
+            String driverClassName = properties.getProperty("db.driverClassName");
+            Class.forName(driverClassName);
 
+            connection = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
+        } catch (SQLException e) {
+            throw new IllegalStateException(e);
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        } catch (ClassNotFoundException e) {
+            throw new IllegalStateException(e);
+        }
     }
 
     @Override
