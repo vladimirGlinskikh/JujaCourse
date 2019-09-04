4c005b3845a4ad3c6379d5ee441f0237bdddb2df
VladimirGlinskikh <support@zhelezyaka.kz>
2019-09-04 23:07:48 +0600

    created UsersServlet class and add dependencies spring-jdbc 4.3.15 version

---
+++ src/main/java/dataBaseExample/kz/zhelezyaka/servlets/UsersServletWithDao.java
@@ -0,0 +1,52 @@
+package dataBaseExample.kz.zhelezyaka.servlets;
+
+import dataBaseExample.kz.zhelezyaka.dao.UsersDao;
+import dataBaseExample.kz.zhelezyaka.dao.UsersDaoJdbcImpl;
+import dataBaseExample.kz.zhelezyaka.models.User;
+import org.apache.catalina.LifecycleState;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.List;
+import java.util.Properties;
+
+@WebServlet("/users")
+public class UsersServletWithDao extends HttpServlet {
+    private UsersDao usersDao;
+
+    @Override
+    public void init() throws ServletException {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        Properties properties = new Properties();
+        try {
+
+            properties.load(new FileInputStream(getServletContext().getRealPath("/WEB-INF/classes/db.properties")));
+            String dbUrl = properties.getProperty("db.url");
+            String dbUsername = properties.getProperty("db.username");
+            String dbPassword = properties.getProperty("db.password");
+            String driverClassName = properties.getProperty("db.driverClassName");
+
+            dataSource.setUsername(dbUsername);
+            dataSource.setPassword(dbPassword);
+            dataSource.setUrl(dbUrl);
+            dataSource.setDriverClassName(driverClassName);
+
+            usersDao = new UsersDaoJdbcImpl(dataSource);
+        } catch (IOException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<User> users = usersDao.findAll();
+        req.setAttribute("usersFromServer", users);
+        req.getServletContext().getRequestDispatcher("/WEB-INF/JSP/users.jsp").forward(req, resp);
+    }
+}
