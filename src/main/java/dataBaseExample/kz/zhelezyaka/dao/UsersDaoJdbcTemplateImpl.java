72cd95ca0e2b39fc207954b7f971640c8bcb95e4
VladimirGlinskikh <support@zhelezyaka.kz>
2019-09-06 17:53:18 +0600

    created class Car and exchange interface RawMapper

--- src/main/java/dataBaseExample/kz/zhelezyaka/dao/UsersDaoJdbcTemplateImpl.java
+++ src/main/java/dataBaseExample/kz/zhelezyaka/dao/UsersDaoJdbcTemplateImpl.java
@@ -1,16 +1,19 @@
 package dataBaseExample.kz.zhelezyaka.dao;
 
+import dataBaseExample.kz.zhelezyaka.models.Car;
 import dataBaseExample.kz.zhelezyaka.models.User;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 
 import javax.sql.DataSource;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 public class UsersDaoJdbcTemplateImpl implements UsersDao {
     private JdbcTemplate template;
+    private Map<Integer, User> usersMap = new HashMap<>();
     private final String SQL_SELECT_ALL = "SELECT * FROM fix_user";
+    //language=SQL
+    private final String SQL_SELECT_USER_CARS = "SELECT fix_user.*, fix_car.id AS car_id, fix_car.model FROM fix_user LEFT JOIN fix_car ON fix_user.id = fix_car.owner_id WHERE fix_user.id = ?";
     //language=SQL
     private final String SQL_SELECT_ALL_BY_FIRST_NAME = "SELECT *FROM fix_user WHERE first_name = ?";
 
@@ -18,10 +21,19 @@ public class UsersDaoJdbcTemplateImpl implements UsersDao {
         this.template = new JdbcTemplate(dataSource);
     }
 
-    private RowMapper<User> userRowMapper = (resultSet, i) -> new User(
-            resultSet.getInt("id"),
-            resultSet.getString("first_name"),
-            resultSet.getString("last_name"));
+    private RowMapper<User> userRowMapper = (resultSet, i) -> {
+        Integer id = resultSet.getInt("id");
+        if (!usersMap.containsKey(id)) {
+            String firstName = resultSet.getString("first_name");
+            String lastName = resultSet.getString("last_name");
+            User user = new User(id, firstName, lastName, new ArrayList<>());
+            usersMap.put(id, user);
+        }
+        Car car = new Car(resultSet.getInt("car_id"),
+                resultSet.getString("model"), usersMap.get(id));
+        usersMap.get(id).getCars().add(car);
+        return usersMap.get(id);
+    };
 
     @Override
     public List<User> findAllByFirstName(String firstName) {
--- src/main/java/dataBaseExample/kz/zhelezyaka/models/User.java
+++ src/main/java/dataBaseExample/kz/zhelezyaka/models/User.java
@@ -1,22 +1,35 @@
 package dataBaseExample.kz.zhelezyaka.models;
 
+import java.util.List;
+
 public class User {
     private Integer id;
     private String firstName;
     private String lastName;
+    private List<Car> cars;
 
     public User() {
-    }
-
-    public User(String firstName, String lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
     }
 
     public User(Integer id, String firstName, String lastName) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
+    }
+
+    public User(Integer id, String firstName, String lastName, List<Car> cars) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.cars = cars;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
     }
 
     public String getFirstName() {
@@ -34,6 +47,44 @@ public class User {
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public void setCars(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    //    public User() {
+//    }
+//
+//    public User(String firstName, String lastName) {
+//        this.firstName = firstName;
+//        this.lastName = lastName;
+//    }
+//
+//    public User(Integer id, String firstName, String lastName) {
+//        this.id = id;
+//        this.firstName = firstName;
+//        this.lastName = lastName;
+//    }
+//
+//    public String getFirstName() {
+//        return firstName;
+//    }
+//
+//    public void setFirstName(String firstName) {
+//        this.firstName = firstName;
+//    }
+//
+//    public String getLastName() {
+//        return lastName;
+//    }
+//
+//    public void setLastName(String lastName) {
+//        this.lastName = lastName;
+//    }
 
 
 }
