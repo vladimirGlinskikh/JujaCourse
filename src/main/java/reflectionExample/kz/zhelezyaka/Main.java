1a39f9439efa2ade8641792d26368c5adac8e874
VladimirGlinskikh <support@zhelezyaka.kz>
2019-09-07 22:44:33 +0600

    primitive example work reflection API

---
+++ src/main/java/reflectionExample/kz/zhelezyaka/Main.java
@@ -0,0 +1,12 @@
+package reflectionExample.kz.zhelezyaka;
+
+import java.lang.reflect.Field;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        SomeClass someObject = new SomeClass();
+        Class<SomeClass> someClassAsClass = (Class<SomeClass>) someObject.getClass();
+        Field someField = someClassAsClass.getField("someField");
+        System.out.println(someField.getType());
+    }
+}
---
+++ src/main/java/reflectionExample/kz/zhelezyaka/SomeClass.java
@@ -0,0 +1,5 @@
+package reflectionExample.kz.zhelezyaka;
+
+public class SomeClass {
+    public int someField;
+}
