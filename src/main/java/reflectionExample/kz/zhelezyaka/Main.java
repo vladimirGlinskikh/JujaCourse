e05bdedb7c54ca2dc115d6cc016e98ddefe4bae1
VladimirGlinskikh <support@zhelezyaka.kz>
2019-09-09 23:43:17 +0600

    Displaying fields using Reflection

--- src/main/java/reflectionExample/kz/zhelezyaka/Main.java
+++ src/main/java/reflectionExample/kz/zhelezyaka/Main.java
@@ -8,5 +8,9 @@ public class Main {
         Class<SomeClass> someClassAsClass = (Class<SomeClass>) someObject.getClass();
         Field someField = someClassAsClass.getField("someField");
         System.out.println(someField.getType());
+        Field[] fields = someClassAsClass.getFields();
+        for (Field field : fields) {
+            System.out.println(field.getType() + " " + field.getName());
+        }
     }
 }
--- src/main/java/reflectionExample/kz/zhelezyaka/SomeClass.java
+++ src/main/java/reflectionExample/kz/zhelezyaka/SomeClass.java
@@ -2,4 +2,5 @@ package reflectionExample.kz.zhelezyaka;
 
 public class SomeClass {
     public int someField;
+    public String someString;
 }
