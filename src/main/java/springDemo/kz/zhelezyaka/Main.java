47466fc55b9f62953c8a8fd964d52ef42f6a3cc9
VladimirGlinskikh <support@zhelezyaka.kz>
2019-09-12 23:41:34 +0600

    created class Main and connected checked

--- src/main/java/springDemo/kz/zhelezyaka/Main.java
+++ src/main/java/springDemo/kz/zhelezyaka/Main.java
@@ -1,4 +1,11 @@
 package springDemo.kz.zhelezyaka;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("kz.zhelezyaka/context.xml");
+    }
 }
