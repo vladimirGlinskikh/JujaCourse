e574f820f9a4cbfab3c7e8e3bb2c9daf64524350
VladimirGlinskikh <support@zhelezyaka.kz>
2019-09-15 21:42:05 +0600

    created class User for models

---
+++ src/main/java/springMVCExample/kz/zhelezyaka/models/User.java
@@ -0,0 +1,17 @@
+package springMVCExample.kz.zhelezyaka.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    private long id;
+    private String firstName;
+    private String lastName;
+
+}
