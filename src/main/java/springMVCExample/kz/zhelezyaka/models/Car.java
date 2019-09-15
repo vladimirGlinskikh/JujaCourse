3c1fe7380718c15cdb49e53dd76d593047072bc1
VladimirGlinskikh <support@zhelezyaka.kz>
2019-09-15 21:45:16 +0600

    created class Car for models

---
+++ src/main/java/springMVCExample/kz/zhelezyaka/models/Car.java
@@ -0,0 +1,16 @@
+package springMVCExample.kz.zhelezyaka.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Car {
+    private long id;
+    private String model;
+    private User owner;
+}
