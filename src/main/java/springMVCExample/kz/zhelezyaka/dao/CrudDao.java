f350ad5d50f29d8b24dc924d7d84dadecd07ee15
VladimirGlinskikh <support@zhelezyaka.kz>
2019-09-15 21:49:46 +0600

    created interface CrudDao for dao

---
+++ src/main/java/springMVCExample/kz/zhelezyaka/dao/CrudDao.java
@@ -0,0 +1,16 @@
+package springMVCExample.kz.zhelezyaka.dao;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CrudDao<T> {
+    Optional<T> find(Integer id);
+
+    void save(T model);
+
+    void update(T model);
+
+    void delete(Integer id);
+
+    List<T> findAll();
+}
--- src/main/java/springMVCExample/kz/zhelezyaka/models/User.java
+++ src/main/java/springMVCExample/kz/zhelezyaka/models/User.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -14,4 +16,5 @@ public class User {
     private String firstName;
     private String lastName;
 
+    private List<Car> cars;
 }
