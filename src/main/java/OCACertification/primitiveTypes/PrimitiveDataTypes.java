8f0848a15ee24572b9a35ddc5d66ecd75fc4c2d4
VladimirGlinskikh <support@zhelezyaka.kz>
2019-10-06 00:13:50 +0600

    add class PrimitiveDataTypes

---
+++ src/main/java/OCACertification/primitiveTypes/PrimitiveDataTypes.java
@@ -0,0 +1,18 @@
+package OCACertification.primitiveTypes;
+
+public class PrimitiveDataTypes {
+    public static void main(String[] args) {
+        byte b1 = 10;
+        byte b2 = 20;
+        byte b3 = 127;
+
+        short s1 = 5;
+        short s2 = -10;
+        short s3 = 0;
+
+        int i1 = 500;
+
+        long l1 = 1000;
+        long l2 = 10_000_000_000L;
+    }
+}
