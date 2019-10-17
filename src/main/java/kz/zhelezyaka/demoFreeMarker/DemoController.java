b3522f491314a11cde1840ee2e4c45fcb6210f36
VladimirGlinskikh <support@zhelezyaka.kz>
2019-10-17 12:33:39 +0600

    class DemoController and DemoFreeMarkerApplication

--- src/main/java/kz/zhelezyaka/demoFreeMarker/DemoController.java
+++ src/main/java/kz/zhelezyaka/demoFreeMarker/DemoController.java
@@ -1,4 +1,38 @@
 package kz.zhelezyaka.demoFreeMarker;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Controller
 public class DemoController {
+    @Autowired
+    private RepositoryService repositoryService;
+
+    @GetMapping("/")
+    public String index() {
+        return "persons";
+    }
+
+    @RequestMapping(value = "/search", method = RequestMethod.POST)
+    public String hello(Model model, @RequestParam(defaultValue = "") String searchName) {
+        List<List<String>> persons = repositoryService.getRepository();
+        List<List<String>> filterList = persons.stream()
+                .filter(p -> p.get(0).contains(searchName))
+                .collect(Collectors.toList());
+        model.addAttribute("persons", filterList);
+        model.addAttribute("lastSearch", searchName);
+        return "persons";
+    }
+
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String save(Model model, @ModelAttribute("person") Person person) {
+        List<List<String>> persons = repositoryService.addPerson(person);
+        model.addAttribute("persons", persons);
+        return "persons";
+    }
 }
--- src/main/java/kz/zhelezyaka/demoFreeMarker/DemoFreeMarkerApplication.java
+++ src/main/java/kz/zhelezyaka/demoFreeMarker/DemoFreeMarkerApplication.java
@@ -1,4 +1,11 @@
 package kz.zhelezyaka.demoFreeMarker;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class DemoFreeMarkerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoFreeMarkerApplication.class, args);
+    }
 }
