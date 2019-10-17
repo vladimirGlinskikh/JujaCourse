798ea3446024b275f929a1bf0de37f1e233d918d
VladimirGlinskikh <support@zhelezyaka.kz>
2019-10-17 12:28:20 +0600

    class RepositoryService

--- src/main/java/kz/zhelezyaka/demoFreeMarker/RepositoryService.java
+++ src/main/java/kz/zhelezyaka/demoFreeMarker/RepositoryService.java
@@ -1,4 +1,21 @@
 package kz.zhelezyaka.demoFreeMarker;
 
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
 public class RepositoryService {
+    private static List<List<String>> repository = new ArrayList<>();
+
+    public List<List<String>> getRepository() {
+        return repository;
+    }
+
+    public List<List<String>> addPerson(Person person) {
+        repository.add(Arrays.asList(person.getFirstName(), person.getAge().toString()));
+        return repository;
+    }
 }
