241f8c46be013b710f94b60371ef989fd39f4bb2
VladimirGlinskikh <support@zhelezyaka.kz>
2019-10-17 12:27:19 +0600

    class Person

--- src/main/java/kz/zhelezyaka/demoFreeMarker/Person.java
+++ src/main/java/kz/zhelezyaka/demoFreeMarker/Person.java
@@ -1,4 +1,19 @@
 package kz.zhelezyaka.demoFreeMarker;
 
 public class Person {
+    public Person(String firstName, Integer age) {
+        this.firstName = firstName;
+        this.age = age;
+    }
+
+    private String firstName;
+    private Integer age;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
 }
