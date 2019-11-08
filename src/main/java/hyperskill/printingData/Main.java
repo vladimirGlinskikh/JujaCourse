056346c5aa2e2fc08eaa26cd0f10aa4800a6b769
VladimirGlinskikh <support@zhelezyaka.kz>
2019-11-08 22:44:11 +0600

    printing data is ready

---
+++ src/main/java/hyperskill/printingData/Main.java
@@ -0,0 +1,10 @@
+package hyperskill.printingData;
+
+public class Main {
+    public static void main(String[] args) {
+        // put your code here
+        for (int i = 9; i > -1; i--) {
+            System.out.print(i + " ");
+        }
+    }
+}
---
+++ src/main/java/hyperskill/printingData/task.md
@@ -0,0 +1,5 @@
+Write a program that prints numbers from 0 to 9, but in the backward order. All numbers must be in the same line. Separate them by a space. 
+
+The output should be the following:
+
+9 8 7 6 5 4 3 2 1 0
