b16b218683f2c44b76c4387526982496d832417c
VladimirGlinskikh <support@zhelezyaka.kz>
2019-11-08 23:04:00 +0600

    tic tac toe is ready

---
+++ src/main/java/hyperskill/printingData/ticTacToe/Main.java
@@ -0,0 +1,9 @@
+package hyperskill.printingData.ticTacToe;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("0 X X");
+        System.out.println("0 X 0");
+        System.out.println("X 0 X");
+    }
+}
---
+++ src/main/java/hyperskill/printingData/ticTacToe/task.md
@@ -0,0 +1,8 @@
+You need to write a program to display the resulting grid of a Tic-Tac-Toe game.
+
+The grid is shown below.
+
+    
+    |0|X|X|
+    |0|X|0|
+    |X|0|X|
