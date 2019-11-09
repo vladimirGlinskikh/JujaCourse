855be6ca5c5b5946dec1a61ccb6e1843adb1a182
VladimirGlinskikh <support@zhelezyaka.kz>
2019-11-09 22:25:14 +0600

    check

--- src/main/java/hyperskill/printingData/encrypted/Main.java
+++ src/main/java/hyperskill/printingData/encrypted/Main.java
@@ -3,6 +3,10 @@ package hyperskill.printingData.encrypted;
 public class Main {
     public static void main(String[] args) {
 //        "we found a treasure!"
-        System.out.println("dv ulfmw z givzhfiv!");
+//        System.out.println("dv ulfmw z givzhfiv!");
+        // it is wrong char[] array = new char[-1];
+        //char[] array = {'a', 'b', 'c', 'd'};
+        char[] array = new char[1];
+        System.out.println(array.toString());
     }
 }
