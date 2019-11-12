6d21d0f21bbb6a994566b18226abfba5d72d4707
VladimirGlinskikh <support@zhelezyaka.kz>
2019-11-12 23:50:34 +0600

    check

--- src/main/java/hyperskill/printingData/encrypted/Main.java
+++ src/main/java/hyperskill/printingData/encrypted/Main.java
@@ -1,12 +1,22 @@
 package hyperskill.printingData.encrypted;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-//        "we found a treasure!"
-//        System.out.println("dv ulfmw z givzhfiv!");
-        // it is wrong char[] array = new char[-1];
-        //char[] array = {'a', 'b', 'c', 'd'};
-        char[] array = new char[1];
-        System.out.println(array.toString());
+
+        Scanner scanner = new Scanner(System.in);
+
+        int hour = scanner.nextInt();
+        int minute = scanner.nextInt();
+        int seconds = scanner.nextInt();
+
+        int h = scanner.nextInt();
+        int m = scanner.nextInt();
+        int s = scanner.nextInt();
+
+        int sum = (hour * 3600) + (minute * 60) + seconds;
+        int summ = (h * 3600) + (m * 60) + s;
+        System.out.println(summ - sum);
     }
 }
