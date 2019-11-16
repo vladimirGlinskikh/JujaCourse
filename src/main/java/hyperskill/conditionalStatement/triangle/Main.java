4bcbcb98eab96ae17e70b0eae9d65655fbd44da9
VladimirGlinskikh <support@zhelezyaka.kz>
2019-11-16 22:24:56 +0600

    check

---
+++ src/main/java/hyperskill/conditionalStatement/triangle/Main.java
@@ -0,0 +1,19 @@
+package hyperskill.conditionalStatement.triangle;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+
+        if ((a + b >= c) && (b + c > a) && (a + c > b)) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
---
+++ src/main/java/hyperskill/conditionalStatement/triangle/task.md
@@ -0,0 +1,13 @@
+Given three natural numbers A, B, C. Determine if a triangle with these sides can exist.
+
+If the triangle exists - output the YES string, otherwise - output NO.
+
+Note, a triangle is formed by three connected points that are not located on a single straight line.
+
+Sample Input 1:<br/>
+3<br/>
+4<br/>
+5
+
+Sample Output 1:<br/>
+YES
