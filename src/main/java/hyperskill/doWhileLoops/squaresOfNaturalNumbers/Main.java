ef3056fe5c1d090b97460080f3f2de5b8355d219
VladimirGlinskikh <support@zhelezyaka.kz>
2019-11-20 14:44:34 +0600

    check

---
+++ src/main/java/hyperskill/doWhileLoops/squaresOfNaturalNumbers/Main.java
@@ -0,0 +1,21 @@
+package hyperskill.doWhileLoops.squaresOfNaturalNumbers;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int square = 0;
+        int prev = 0;
+
+        for (int i = 1; prev < n; i++) {
+            square = (square + i + prev);
+            if (square <= n) {
+                System.out.println(square);
+            }
+            prev = i;
+        }
+    }
+}
---
+++ src/main/java/hyperskill/doWhileLoops/squaresOfNaturalNumbers/task.md
@@ -0,0 +1,13 @@
+Given integer N. Print all the squares of natural numbers, not exceeding N, in ascending order.
+
+Sample Input 1:<br>
+50
+
+Sample Output 1:<br>
+1<br>
+4<br>
+9<br>
+16<br>
+25<br>
+36<br>
+49
