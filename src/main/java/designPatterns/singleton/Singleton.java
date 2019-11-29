190d135958523556a5421a7ebabc49bbaeccb950
VladimirGlinskikh <support@zhelezyaka.kz>
2019-11-29 22:14:52 +0600

    the simplest implementation of the "Singleton" pattern

---
+++ src/main/java/designPatterns/singleton/Singleton.java
@@ -0,0 +1,14 @@
+package designPatterns.singleton;
+
+public final class Singleton {
+    private static Singleton instance = null;
+
+    private Singleton() {
+    }
+
+    public static synchronized Singleton getInstance() {
+        if (instance == null)
+            instance = new Singleton();
+        return instance;
+    }
+}
