21030b3ede176f2bf3b85569f498aca00917aa4a
VladimirGlinskikh <support@zhelezyaka.kz>
2019-12-02 21:18:20 +0600

    create pattern Builder

--- src/main/java/designPatterns/builder/Person.java
+++ src/main/java/designPatterns/builder/Person.java
@@ -6,4 +6,41 @@ public class Person {
     private int age;
     private int height;
     private int weight;
+
+    public static class Builder {
+        private Person newPerson;
+
+        public Builder() {
+            newPerson = new Person();
+        }
+
+        public Builder withName(String name) {
+            newPerson.name = name;
+            return this;
+        }
+
+        public Builder withSurname(String surname) {
+            newPerson.surname = surname;
+            return this;
+        }
+
+        public Builder withAge(int age) {
+            newPerson.age = age;
+            return this;
+        }
+
+        public Builder withHeight(int height) {
+            newPerson.height = height;
+            return this;
+        }
+
+        public Builder withWidth(int weight) {
+            newPerson.weight = weight;
+            return this;
+        }
+
+        public Person build() {
+            return newPerson;
+        }
+    }
 }
