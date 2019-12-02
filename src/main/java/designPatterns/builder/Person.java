81bbefc67df57151330546d9c45d3d32d7b37e9c
VladimirGlinskikh <support@zhelezyaka.kz>
2019-12-02 21:12:08 +0600

    create class Person

---
+++ src/main/java/designPatterns/builder/Person.java
@@ -0,0 +1,9 @@
+package designPatterns.builder;
+
+public class Person {
+    private String name;
+    private String surname;
+    private int age;
+    private int height;
+    private int weight;
+}
