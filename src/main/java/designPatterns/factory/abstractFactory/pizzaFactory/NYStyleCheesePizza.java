78f1663f4ae1afa670a63081720cb40f08d3551c
VladimirGlinskikh <support@zhelezyaka.kz>
2019-12-10 21:56:08 +0600

    New York Pizza baked with sauce marinare

---
+++ src/main/java/designPatterns/factory/abstractFactory/pizzaFactory/NYStyleCheesePizza.java
@@ -0,0 +1,11 @@
+package designPatterns.factory.abstractFactory.pizzaFactory;
+
+public class NYStyleCheesePizza extends Pizza {
+    public NYStyleCheesePizza() {
+        name = "NY Style Sauce and Cheese Pizza";
+        dough = "Thin Crust Dough";
+        sauce = "Marinara Sauce";
+
+        toppings.add("Granted Reggiano Cheese");
+    }
+}
