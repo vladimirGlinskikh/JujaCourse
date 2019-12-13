2152ee5d2fbdfcf22549cc46d4eb179eddc58bd4
VladimirGlinskikh <support@zhelezyaka.kz>
2019-12-13 16:46:26 +0600

    check

---
+++ src/main/java/hyperskill/functional/Account.java
@@ -0,0 +1,10 @@
+package hyperskill.functional;
+
+import java.util.List;
+
+public class Account {
+//    List<Account> nonEmptyAccounts =
+//            filter(accounts, account -> account.balance > 0);
+//    List<Account> accountsWithTooMuchMoney =
+//            filter(accounts, account -> !account.isLocked() && account.balance >= 100_000_000);
+}
