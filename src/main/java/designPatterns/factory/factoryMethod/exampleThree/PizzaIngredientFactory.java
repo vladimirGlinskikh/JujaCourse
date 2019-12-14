499282fb7cf9d18f716a3907f781b9179ad22656
VladimirGlinskikh <support@zhelezyaka.kz>
2019-12-14 21:50:44 +0600

    defined a factory interface that will create all the ingredients

---
+++ src/main/java/designPatterns/factory/factoryMethod/exampleThree/PizzaIngredientFactory.java
@@ -0,0 +1,15 @@
+package designPatterns.factory.factoryMethod.exampleThree;
+
+public interface PizzaIngredientFactory {
+    Dough createDough();
+
+    Sauce createSauce();
+
+    Cheese createCheese();
+
+    Veggies[] createVeggies();
+
+    Pepperoni createPepperoni();
+
+    Clams createClam();
+}
