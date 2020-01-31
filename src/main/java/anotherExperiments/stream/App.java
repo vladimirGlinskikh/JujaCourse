b190b6736c4444c68b5f929a2868138de23043a0
VladimirGlinskikh <support@zhelezyaka.kz>
2020-01-31 22:36:45 +0600

    Collection.removeIf(Predicate<? super E> filter) The method iterates over the collection, and removes those elements that match the filter.

--- src/main/java/anotherExperiments/stream/App.java
+++ src/main/java/anotherExperiments/stream/App.java
@@ -7,6 +7,7 @@ import java.util.List;
 public class App {
     public static void main(String[] args) {
         List<Integer> numbers = new ArrayList<>(Arrays.asList(2, 4, 1, 7, 4, 0, 8, -1));
+        numbers.removeIf(n -> n < 3);
         numbers.forEach(n -> System.out.print(n + " "));
     }
 }
