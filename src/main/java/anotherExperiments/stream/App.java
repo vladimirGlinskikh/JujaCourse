7c59fe03f98cf062ce235a73bccb09508c6781e9
VladimirGlinskikh <support@zhelezyaka.kz>
2020-01-31 22:33:52 +0600

    Iterable.forEach(Consumer<? super T> action) Iterates over the passed collection, and executes the action lambda expression for each of its elements.

---
+++ src/main/java/anotherExperiments/stream/App.java
@@ -0,0 +1,12 @@
+package anotherExperiments.stream;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>(Arrays.asList(2, 4, 1, 7, 4, 0, 8, -1));
+        numbers.forEach(n -> System.out.print(n + " "));
+    }
+}
