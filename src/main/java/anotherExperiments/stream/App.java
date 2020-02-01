d0930015680f0f50a702158ecd994975ee64d527
VladimirGlinskikh <support@zhelezyaka.kz>
2020-02-01 21:44:42 +0600

    Map.forEach(BiConsumer<? super K, ? super V> action)

--- src/main/java/anotherExperiments/stream/App.java
+++ src/main/java/anotherExperiments/stream/App.java
@@ -1,13 +1,19 @@
 package anotherExperiments.stream;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class App {
     public static void main(String[] args) {
-        List<Integer> numbers = new ArrayList<>(Arrays.asList(2, 4, 1, 7, 4, 0, 8, -1));
-        numbers.removeIf(n -> n < 3);
-        numbers.forEach(n -> System.out.print(n + " "));
+//        List<Integer> numbers = new ArrayList<>(Arrays.asList(2, 4, 1, 7, 4, 0, 8, -1));
+//        numbers.removeIf(n -> n < 3);
+//        numbers.forEach(n -> System.out.print(n + " "));
+
+        Map<String, String> books = new HashMap<>();
+        books.put("Война и мир", "Лев Толстой");
+        books.put("Преступление и наказание", "Федор Достоевский");
+        books.put("Философия Java", "Брюс Эккель");
+        books.put("Братья Карамазовы", "Федор Достоевский");
+        books.put("Властелин Колец", "Джон Толкин");
+        books.forEach((a, b) -> System.out.println("Название книги: " + a + ". Автор: " + b));
     }
 }
