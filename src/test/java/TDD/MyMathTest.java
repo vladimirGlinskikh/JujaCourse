2af89128abf2a2a4902629acf7257ab580402335
VladimirGlinskikh <support@zhelezyaka.kz>
2020-02-02 22:38:09 +0600

    example test with Parameterized.Parameters

--- src/test/java/TDD/MyMathTest.java
+++ src/test/java/TDD/MyMathTest.java
@@ -1,18 +1,35 @@
 package TDD;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
+import org.assertj.core.util.Arrays;
+import org.junit.*;
 import org.junit.rules.TestRule;
 import org.junit.rules.Timeout;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Collection;
 
 import static org.junit.Assert.*;
 
+@RunWith(Parameterized.class)
 public class MyMathTest {
+    int a, b, expResult;
+
+    public MyMathTest(int a, int b, int expResult) {
+        this.a = a;
+        this.b = b;
+        this.expResult = expResult;
+    }
 
     @Rule
     public TestRule timeout = new Timeout(300);
+
+    @Parameterized.Parameters
+    public static Collection numbers() {
+        return Arrays.asList(new Object[][]{
+                {1, 3, 4}, {2, 3, 5}, {2, 4, 6}
+        });
+    }
 
     @Before
     public void setUp() throws Exception {
@@ -24,13 +41,11 @@ public class MyMathTest {
 
     @Test
     public void add() throws InterruptedException {
-        int a = 2;
-        int b = 7;
-        int expResult = 9;
         int result = MyMath.add(a, b);
         assertEquals(expResult, result);
     }
 
+    @Ignore
     @Test(expected = ArithmeticException.class)
     public void div() throws InterruptedException {
         int a = 7;
