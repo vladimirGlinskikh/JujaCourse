52cbdb78a38018e4e46dae40cb87c49e1ec4b2d8
VladimirGlinskikh <support@zhelezyaka.kz>
2020-02-06 02:28:37 +0600

    example with Mockito

---
+++ src/main/java/TDD/SimpleMock.java
@@ -0,0 +1,15 @@
+package TDD;
+
+import org.mockito.Mockito;
+
+import java.util.List;
+
+public class SimpleMock {
+    public static void main(String[] args) {
+        List<String> list = Mockito.mock(List.class);
+        System.out.println(list.add("A"));
+        System.out.println(list.get(-1));
+        System.out.println(list.size());
+        System.out.println(list.iterator());
+    }
+}
