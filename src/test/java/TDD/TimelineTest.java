24dd2277f6d9951148d07360cb5d251d0df4e9c2
VladimirGlinskikh <support@zhelezyaka.kz>
2020-02-06 18:35:27 +0600

    we put Timeline into the setUp method

--- src/test/java/TDD/TimelineTest.java
+++ src/test/java/TDD/TimelineTest.java
@@ -1,5 +1,6 @@
 package TDD;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -7,22 +8,21 @@ import static org.junit.Assert.assertTrue;
 
 public class TimelineTest {
     private final static int NEW_FETCH_COUNT = new Timeline().getFetchCount() + 1;
+    private Timeline timeline;
+
+    @Before
+    public void setUp() {
+        timeline = new Timeline();
+    }
 
     @Test
     public void setFetchCount() {
-        Timeline timeline = new Timeline();
-
         timeline.setFetchCount(NEW_FETCH_COUNT);
         assertEquals(NEW_FETCH_COUNT, timeline.getFetchCount());
     }
 
     @Test
     public void initialState() {
-        Timeline timeline = createTimeline();
         assertTrue(timeline.getFetchCount() > 0);
-    }
-
-    private static Timeline createTimeline() {
-        return new Timeline();
     }
 }
