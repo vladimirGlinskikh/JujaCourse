2cc611016aae7fc7b1755b5293d5bd761eab03cf
VladimirGlinskikh <support@zhelezyaka.kz>
2020-02-06 17:34:20 +0600

    created special delegation

--- src/test/java/TDD/TimelineTest.java
+++ src/test/java/TDD/TimelineTest.java
@@ -3,9 +3,10 @@ package TDD;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class TimelineTest {
-    private final static int NEW_FETCH_COUNT = Timeline.DEFAULT_FETCH_COUNT + 1;
+    private final static int NEW_FETCH_COUNT = new Timeline().getFetchCount() + 1;
 
     @Test
     public void setFetchCount() {
@@ -13,5 +14,15 @@ public class TimelineTest {
 
         timeline.setFetchCount(NEW_FETCH_COUNT);
         assertEquals(NEW_FETCH_COUNT, timeline.getFetchCount());
+    }
+
+    @Test
+    public void initialState() {
+        Timeline timeline = createTimeline();
+        assertTrue(timeline.getFetchCount() > 0);
+    }
+
+    private static Timeline createTimeline() {
+        return new Timeline();
     }
 }
